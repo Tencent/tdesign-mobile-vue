1ea83abb75a836addca4ecb48f0b9439a5888853
ppYoung <414400938@qq.com>
2020-05-22 15:28:28 +0800

    chore(picker): 增加picker组件和子组件picker-column

--- babel.config.js
+++ babel.config.js
@@ -5,19 +5,19 @@ module.exports = {
   // plugins: [transformJsx]
   presets: [
     [
-      "@babel/preset-env",
+      '@babel/preset-env',
       {
         targets: {
           browsers: [
-            "last 3 Chrome versions",
-            "last 3 Firefox versions",
-            "Safari >= 10",
-            "Explorer >= 11",
-            "Edge >= 12",
+            'last 3 Chrome versions',
+            'last 3 Firefox versions',
+            'Safari >= 10',
+            'Explorer >= 11',
+            'Edge >= 12',
           ],
         },
         modules: false,
-      }
-    ]
-  ]
+      },
+    ],
+  ],
 };
---
+++ src/picker/index.ts
@@ -0,0 +1,7 @@
+import PickerColumn from './picker-column.vue';
+import Picker from './picker.vue';
+
+export {
+  PickerColumn,
+  Picker,
+};
