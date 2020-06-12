a7c0b1b3fae292aa1cb0b4bbb971a3d06d06c6ca
JosonYang <258490116@qq.com>
2020-06-12 19:31:28 +0800

    fix(docs): change webhistory to webhashhistory

--- docs/sites/router.ts
+++ docs/sites/router.ts
@@ -1,4 +1,4 @@
-import { RouteRecordRaw, createRouter, createWebHistory } from 'vue-router';
+import { RouteRecordRaw, createRouter, createWebHashHistory } from 'vue-router';
 import config from './sites.config';
 
 // const demoReq = require.context("@/", true, /demos[/\\][\w-]+\.vue$/im);
@@ -34,7 +34,7 @@ const routes: Array<RouteRecordRaw> = [
   ...getDocsRoutes(navs.components.docs, 'component'),
 ];
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes,
 });
 
