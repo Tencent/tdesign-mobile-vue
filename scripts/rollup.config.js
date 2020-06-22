534345a061fa554a714ddcdc42505b63bc3735ae
BuptStEve <yangzhenyu2016@gmail.com>
2020-06-22 11:56:35 +0800

    build: support 'import on demand', there're 3 output dirs: dist/es/lib

--- scripts/rollup.config.js
+++ scripts/rollup.config.js
@@ -1,10 +1,11 @@
 import url from '@rollup/plugin-url';
-import path from 'path';
 import json from '@rollup/plugin-json';
 import babel from '@rollup/plugin-babel';
 import postcss from 'rollup-plugin-postcss';
 import replace from '@rollup/plugin-replace';
+import analyzer from 'rollup-plugin-analyzer';
 import vuePlugin from 'rollup-plugin-vue';
+import multiInput from 'rollup-plugin-multi-input';
 import typescript from 'rollup-plugin-typescript2';
 import { terser } from 'rollup-plugin-terser';
 import { eslint } from 'rollup-plugin-eslint';
@@ -13,6 +14,8 @@ import { DEFAULT_EXTENSIONS } from '@babel/core';
 import pkg from '../package.json';
 
 const name = 'tdesign';
+const externalDeps = Object.keys(pkg.dependencies);
+const externalPeerDeps = Object.keys(pkg.peerDependencies);
 const banner =
 `/**
  * ${name} v${pkg.version}
@@ -21,75 +24,116 @@ const banner =
  */
 `;
 
-const configMap = {
-  umdDev: {
-    env: 'development',
-    file: `dist/${name}.umd.js`,
-    format: 'umd',
-  },
-  umdProd: {
-    env: 'production',
-    file: `dist/${name}.umd.min.js`,
-    format: 'umd',
-  },
-};
-
-const genConfig = (opts) => {
-  // 生产环境
-  const isProd = /min\.js$/.test(opts.file);
+const getPlugins = ({ env, isProd, analyze, vueOpt = { css: false } }) => {
   const plugins = [
-    eslint({ include: '**/*.ts' }),
+    eslint({ include: ['**/*.ts', '**/*.js'] }),
     typescript({
       cacheRoot: `${require('os').tmpdir()}/.rpt2_cache`,
+      // TODO: typings
       useTsconfigDeclarationDir: true,
     }),
+    vuePlugin(vueOpt),
     babel({
       babelHelpers: 'bundled',
-      extensions: [...DEFAULT_EXTENSIONS, 'ts', 'tsx'],
+      extensions: [...DEFAULT_EXTENSIONS, 'vue', 'ts', 'tsx'],
     }),
-    vuePlugin({ css: false }),
+    // TODO: extract css
     postcss({
-      extract: path.resolve(`dist/${isProd ? `${name}.min` : name}.css`),
+      extract: `${isProd ? `${name}.min` : name}.css`,
       minimize: isProd,
       sourceMap: true,
-      extensions: ['.sass', '.scss', '.css', '.less'],
+      extensions: ['.sass', '.scss', '.css'],
     }),
     json(),
     url(),
   ];
 
-  const config = {
-    input: 'src/index.ts',
-    plugins: plugins.slice(),
-    output: {
-      name,
-      file: opts.file,
-      banner,
-      format: opts.format,
-      exports: 'named',
-      globals: { vue: 'Vue' },
-      sourcemap: true,
-    },
-    external: ['vue'],
-  };
+  if (env) {
+    plugins.push(replace({
+      'process.env.NODE_ENV': JSON.stringify(env),
+    }));
+  }
 
-  if (opts.env) {
-    config.plugins.push(replace({
-      'process.env.NODE_ENV': JSON.stringify(opts.env),
+  if (analyze) {
+    plugins.push(analyzer({
+      limit: 5,
+      summaryOnly: true,
+      ...analyze,
     }));
   }
 
   if (isProd) {
-    config.plugins.push(terser({
+    plugins.push(terser({
       output: {
         ascii_only: true, // eslint-disable
       },
     }));
   }
 
-  return config;
+  return plugins;
 };
 
-export default Object.keys(configMap)
-  .map(key => configMap[key])
-  .map(genConfig);
+const commonConfig = {
+  multi: {
+    input: [
+      'src/**/*.ts',
+      '!src/**/demos',
+      '!src/**/__tests__',
+    ],
+    external: externalDeps.concat(externalPeerDeps),
+    plugins: [multiInput()].concat(getPlugins({ isProd: false, vueOpt: { css: true } })),
+    output: { banner, sourcemap: true },
+  },
+  bundle: {
+    input: 'src/index.ts',
+    external: externalPeerDeps,
+    output: {
+      name: 'TDesign',
+      banner,
+      format: 'umd',
+      exports: 'named',
+      globals: { vue: 'Vue' },
+      sourcemap: true,
+    },
+  },
+};
+
+export default [
+  // esm
+  {
+    ...commonConfig.multi,
+    output: {
+      ...commonConfig.multi.output,
+      dir: 'es/',
+      format: 'esm',
+    },
+  },
+  // cjs
+  {
+    ...commonConfig.multi,
+    output: {
+      ...commonConfig.multi.output,
+      dir: 'lib/',
+      format: 'cjs',
+      exports: 'named',
+    },
+  },
+  // umd
+  {
+    ...commonConfig.bundle,
+    plugins: getPlugins({ isProd: false, env: 'development' }),
+    output: {
+      ...commonConfig.bundle.output,
+      file: 'dist/TDesign.umd.js',
+    },
+  },
+  // umd.min
+  {
+    ...commonConfig.bundle,
+    plugins: getPlugins({ isProd: true, env: 'production', analyze: true }),
+    output: {
+      ...commonConfig.bundle.output,
+      file: 'dist/TDesign.umd.min.js',
+    },
+  },
+];
