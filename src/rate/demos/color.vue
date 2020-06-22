7d612fecec54d4f3801761fbfdb91e98f7d02b92
Nice-PLQ <562714485@qq.com>
2020-06-22 13:10:42 +0800

    feat(rate): add demo docs

---
+++ src/rate/demos/color.vue
@@ -0,0 +1,13 @@
+<template>
+  <t-rate v-model="value" color="#0052d9" />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: 5,
+    };
+  },
+};
+</script>
---
+++ src/rate/demos/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="tdesign-demo-block">
+      <t-rate v-model="value" />
+    </div>
+
+    <div class="tdesign-demo-block">
+      <!-- 允许清除 -->
+      <t-rate v-model="value1" clearable />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: 2,
+      value1: 3,
+    };
+  },
+};
+</script>
