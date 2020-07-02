75dd81d18cdfd4b38cb2b88e65c7c88c468226ea
yorick304 <892390913@qq.com>
2020-07-02 09:29:27 +0800

    docs: 文档更新

--- src/radio/demos/group.vue
+++ src/radio/demos/group.vue
@@ -3,7 +3,7 @@
     <t-radio-group v-model="radio" @change="change">
       <t-radio name="1" title="苹果"></t-radio>
       <t-radio name="2" title="西瓜"></t-radio>
-      <t-radio name="3" title="香蕉" checked-color="#07c160"></t-radio>
+      <t-radio name="3" title="香蕉"></t-radio>
       <t-radio name="4" title="桃子"></t-radio>
       <t-radio name="5" title="雪梨" disabled></t-radio>
     </t-radio-group>
