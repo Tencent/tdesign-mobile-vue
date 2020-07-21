2887c34adb1a454c379dc2fb1cba64de81b87329
JosonYang <258490116@qq.com>
2020-07-21 10:41:41 +0800

    feat: add contributeors in demo page

--- docs/sites/main.ts
+++ docs/sites/main.ts
@@ -2,15 +2,17 @@ import { createApp } from 'vue';
 import app from './app.vue';
 import router from './router';
 import TDesignDemo from './components/demo.vue';
+import ComponentContributors from './components/component-contributors.vue';
 
 import TDesign from '@/index';
 
 import '../../common/style/mobile/index.less';
+import '@/../common/style/site/index.less';
 import '../styles/sites/index.less';
-
 
 createApp(app)
   .use(TDesign)
   .use(router)
   .component('tdesign-demo', TDesignDemo)
+  .component('tdesign-component-contributors', ComponentContributors)
   .mount('#app');
