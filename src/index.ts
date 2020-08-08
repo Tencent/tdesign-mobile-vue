6d86cd6c34c686e0eb7b5ef9b27a8ab47fb8244a
BuptStEve <yangzhenyu2016@gmail.com>
2020-08-08 15:54:20 +0800

    fix: import one style file

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,8 @@ import config from './config';
 import * as plugins from './plugins';
 import * as components from './components';
 
+import '../common/style/mobile/';
+
 const install = function (app: App, option?: object): void {
   const newConfig = { ...config, ...option };
 
