bcd67d7694c6e363eaca8e0c6985330a94edecc3
dufemeng <2314627598@qq.com>
2020-11-30 16:32:15 +0800

    docs(tab-bar): 增加组件注释

--- src/tab-bar/index.ts
+++ src/tab-bar/index.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: loomisli
+ * @Date: 2020-10-17 16:40:08
+ * @LastEditTime: 2020-10-17 02:09:28
+ * @LastEditors: loomisli
+ * @Description: add new component
+ * @FilePath: src/tab-bar/index.ts
+ */
 import TabBar from './tab-bar.vue';
 import TabBarItem from './tab-bar-item.vue';
 
