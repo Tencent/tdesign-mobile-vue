9eb0f04e65a868bc9117196d007ce954a4302336
fayching <435462399@163.com>
2020-12-23 09:57:53 +0800

    feat: 新增tabs组件

--- docs/mobile/mobile.config.ts
+++ docs/mobile/mobile.config.ts
@@ -147,6 +147,11 @@ export default {
           name: 'image-viewer',
           component: () => import('@/image-viewer/demos/base.vue'),
         },
+        {
+          title: 'Tabs 选项卡',
+          name: 'tabs',
+          component: () => import('@/tabs/demos/base.vue'),
+        },
       ],
     },
   },
--- src/components.ts
+++ src/components.ts
@@ -28,4 +28,5 @@ export { default as Badge } from './badge';
 export { default as SegmentControl } from './segment-control';
 export { default as Fab } from './fab';
 export { default as ImageViewer } from './image-viewer';
+export { Tabs, TabPanel } from './tabs';
 
