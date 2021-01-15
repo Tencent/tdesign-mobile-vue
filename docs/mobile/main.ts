6446487f6cdb58c684d70878c5357358dffb7ba7
Nice-PLQ <562714485@qq.com>
2021-01-15 13:39:45 +0800

    chore: vite构建调整

--- docs/mobile/main.ts
+++ docs/mobile/main.ts
@@ -3,7 +3,7 @@ import app from './app.vue';
 import header from './components/header.vue';
 import router from './router';
 
-import TDesign from '/@/index';
+import TDesign from '@/index';
 
 import '../../common/style/mobile/index.less';
 import '../styles/mobile/index.less';
--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,19 @@
 import * as path from 'path';
+import vue from '@vitejs/plugin-vue'
 
 module.exports = {
   alias: {
-    // 键必须以斜线开始和结束
-    '/@/': path.resolve(__dirname, 'src'),
+    '@': path.resolve(__dirname, 'src'),
   },
-  resolvers: [
-    {
-      alias(id: string) {
-        // add slash to particular id, then vite won't resolve it as a module
-        return id.replace(/^@\//, '/@/') 
-      },
-    },
-  ],
-  hostname: '0.0.0.0',
-  port: 16000,
-  open: true,
-  https: false,
-  ssr: false,
-  base: process.env.NODE_ENV === 'development' ? './' : '/vue-mobile/',
-  outDir: 'dist',
+  server: {
+    host: '127.0.0.1',
+    port: 16000,
+    open: '/docs/mobile/index.html#/',
+    https: false,
+  },
+  build: {
+    base: process.env.NODE_ENV === 'development' ? './' : '/vue-mobile/',
+    outDir: 'dist',
+  },
+  plugins: [vue()],
 };
