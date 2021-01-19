9c4953628ae82e7eb1ec5b8d23de13fc2731ac66
Petpanda <wangshoumingimis@163.com>
2021-01-19 15:08:49 +0800

    fix: 解决demo lint问题

--- src/steps/demos/content.vue
+++ src/steps/demos/content.vue
@@ -3,7 +3,7 @@
     <t-cell-group title="竖向更多内容步骤">
       <t-steps v-model="current" direction="vertical">
         <t-step title="已完成步骤" content="可自定义此处内容，可自定义此处内容，可自定义此处内容可自定义此处内容可自定义此处内容。">
-          <template v-slot:extra>
+          <template #extra>
             <img src="//0729-75822.gzc.vod.tencent-cloud.com/site_doc/tdesign-logo.png" alt="">
           </template>
         </t-step>
