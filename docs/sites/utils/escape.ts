d15534bb6209a84bd15beb3dd5870907bc01d9e4
chrysalis1215 <785851812@qq.com>
2021-02-07 11:35:47 +0800

    fix(site): fix escape export

--- docs/sites/utils/escape.ts
+++ docs/sites/utils/escape.ts
@@ -1,17 +1,12 @@
-const html2Escape = (sHtml: string) =>  {
+export const html2Escape = (sHtml: string) =>  {
     return sHtml.replace(/[<>&"]/g,function(c){
       return {'<':'&lt;','>':'&gt;','&':'&amp;','"':'&quot;'}[c];
     });
    }
    
-const escape2Html = (str: string): string =>  {
+export const escape2Html = (str: string): string =>  {
     const arrEntities={'lt':'<','gt':'>','nbsp':' ','amp':'&','quot':'"'};
     return str.replace(/&(lt|gt|nbsp|amp|quot);/ig,function(all,t){
         return arrEntities[t];
     });
-}
-
-export default {
-    html2Escape,
-    escape2Html
 }
