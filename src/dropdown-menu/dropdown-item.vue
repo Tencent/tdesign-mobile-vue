108c2be90464269d3b40c9b1bfbc79723a666c3b
krimeshu <krimeshu@gmail.com>
2021-03-02 17:53:33 +0800

    feat(dropdown-item): 图标改为单独引入

--- src/dropdown-menu/dropdown-item.vue
+++ src/dropdown-menu/dropdown-item.vue
@@ -16,7 +16,7 @@
                     :class="styleDropRadio(option.value)"
                   >
                     <template #checkedIcon>
-                      <t-icon v-if="isCheckedRadio(option.value)" name="check" />
+                      <t-check v-if="isCheckedRadio(option.value)" />
                     </template>
                   </t-radio>
                 </t-cell>
@@ -73,7 +73,7 @@
                         :class="styleTreeRadio(option.value, level)"
                       >
                         <template #checkedIcon>
-                          <t-icon v-if="option.value === treeState.selectList[level]" name="check" />
+                          <t-check v-if="option.value === treeState.selectList[level]" />
                         </template>
                       </t-radio>
                     </t-cell>
@@ -111,7 +111,7 @@
 
 <script lang="ts">
 import { computed, toRefs, ref, reactive, inject, watch, defineComponent, nextTick, SetupContext } from 'vue';
-
+import TCheck from '../icon/check.vue';
 import { DropdownMenuPropsType, DropdownItemProps, DropdownItemPropsType } from './dropdown.interface';
 import config from '../config';
 import TransAniControl from './trans-ani-control';
@@ -121,6 +121,7 @@ const name = `${prefix}-dropdown-item`;
 
 export default defineComponent({
   name,
+  components: { TCheck },
   props: DropdownItemProps,
   emits: ['update:modelValue', 'change', 'open', 'opened', 'close', 'closed'],
   setup(props: DropdownItemPropsType, context: SetupContext) {
