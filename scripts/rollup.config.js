c09c053a5ad0b8ba8c1723970e35e73e34443d9a
BuptStEve <yangzhenyu2016@gmail.com>
2021-04-16 11:23:37 +0800

    build(rollup): remove rollup-plugin-eslint

--- scripts/rollup.config.js
+++ scripts/rollup.config.js
@@ -3,7 +3,6 @@ import { tmpdir } from 'os';
 import url from '@rollup/plugin-url';
 import json from '@rollup/plugin-json';
 import babel from '@rollup/plugin-babel';
-// import eslint from '@rollup/plugin-eslint';
 import esbuild from 'rollup-plugin-esbuild';
 import postcss from 'rollup-plugin-postcss';
 import replace from '@rollup/plugin-replace';
@@ -21,20 +20,12 @@ import pkg from '../package.json';
 const getPlugins = ({
   env,
   isProd = false,
-  analyze = false,
   ignoreLess = true,
   extractCss = false,
 } = {}) => {
-  let plugins = [
-    /** TODO: 由于以下报错，暂时关闭 eslint
-     * 0:0  error  Parsing error: "parserOptions.project" has been set for @typescript-eslint/parser.
-      The file does not match your project config: src/cell-group/cell-group.vue?vue&type=script&lang.ts.
-      The file must be included in at least one of the projects provided
-     */
-    // eslint(),
-    vuePlugin(),
-  ];
+  let plugins = [vuePlugin()];
 
+  // ts
   if (isProd) {
     plugins.push(typescript({
       tsconfig: 'tsconfig.build.json',
@@ -57,6 +48,7 @@ const getPlugins = ({
     url(),
   ]);
 
+  // css
   if (extractCss) {
     plugins.push(postcss({
       extract: `${isProd ? `${name}.min` : name}.css`,
@@ -84,14 +76,6 @@ const getPlugins = ({
       values: {
         'process.env.NODE_ENV': JSON.stringify(env),
       },
-    }));
-  }
-
-  if (analyze) {
-    plugins.push(analyzer({
-      limit: 5,
-      summaryOnly: true,
-      ...analyze,
     }));
   }
 
@@ -157,7 +141,13 @@ const cjsConfig = {
 const umdConfig = {
   input,
   external: externalPeerDeps,
-  plugins: getPlugins({ env: 'development', extractCss: true }),
+  plugins: getPlugins({
+    env: 'development',
+    extractCss: true,
+  }).concat(analyzer({
+    limit: 5,
+    summaryOnly: true,
+  })),
   output: {
     name: 'TDesign',
     banner,
@@ -175,7 +165,6 @@ const umdMinConfig = {
   external: externalPeerDeps,
   plugins: getPlugins({
     isProd: true,
-    analyze: true,
     env: 'production',
     extractCss: true,
   }),
