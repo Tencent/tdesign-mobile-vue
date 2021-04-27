b2bf341435bb08ce1195a2e381548b24f785abef
krimeshu <krimeshu@gmail.com>
2021-04-27 13:20:40 +0800

    feat: 更新双列树形菜单示例和占位文案  --story=864333167 --story=864333091

--- src/dropdown-menu/demos/base.vue
+++ src/dropdown-menu/demos/base.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="dropdown-menu-wrap">
-    <t-cell-group title="下拉菜单样式" >
+    <t-cell-group title="下拉菜单样式">
       <t-cell>
         <t-dropdown-menu>
           <t-dropdown-item v-model="valueS" title="菜单名称" :options="optionsN" />
@@ -15,13 +15,7 @@
       </t-cell>
       <t-cell>
         <t-dropdown-menu>
-          <t-dropdown-item
-            v-model="valueM"
-            title="单列"
-            :options="optionsN"
-            select-mode="multi"
-            options-columns="1"
-          />
+          <t-dropdown-item v-model="valueM" title="单列" :options="optionsN" select-mode="multi" options-columns="1" />
           <t-dropdown-item
             v-model="valueM1"
             title="双列"
@@ -75,48 +69,68 @@
           <t-dropdown-menu>
             <t-dropdown-item
               v-model="treeValue1"
-              title="单选树形菜单"
-              :options="optionsT"
+              title="菜单"
+              :options="optionsT1"
               options-layout="tree"
+              select-mode="multi"
             />
             <t-dropdown-item
               v-model="treeValue2"
-              title="多选树形菜单"
+              title="菜单"
               :options="optionsT2"
               options-layout="tree"
               select-mode="multi"
             />
+            <t-dropdown-item v-model="treeValue3" title="菜单" :options="optionsT3" options-layout="tree" />
+            <t-dropdown-item v-model="treeValue4" title="菜单" :options="optionsT4" options-layout="tree" />
           </t-dropdown-menu>
         </DemoContainer>
       </t-cell>
-       <t-cell>
+      <t-cell>
         <t-dropdown-menu>
           <t-dropdown-item
             v-model="treeValue1"
-            title="单选树形菜单"
-            :options="optionsT"
+            title="菜单"
+            :options="optionsT1"
             options-layout="tree"
+            select-mode="multi"
             disabled
           />
           <t-dropdown-item
             v-model="treeValue2"
-            title="多选树形菜单"
+            title="菜单"
             :options="optionsT2"
             options-layout="tree"
             select-mode="multi"
             disabled
           />
+          <t-dropdown-item v-model="treeValue3" title="菜单" :options="optionsT3" options-layout="tree" disabled />
+          <t-dropdown-item v-model="treeValue4" title="菜单" :options="optionsT4" options-layout="tree" disabled />
         </t-dropdown-menu>
       </t-cell>
     </t-cell-group>
   </div>
-
-
 </template>
 
 <script lang="ts">
 import { ref, defineComponent } from 'vue';
 import DemoContainer from './demo-container.vue';
+
+const buildTree = (length: Number, ...childLengthes: Number[]) => {
+  const tree: any[] = [];
+  for (let i = 0; i < length; i++) {
+    const item: any = {
+      title: `选项 ${i + 1}`,
+      value: `options_${i}`,
+    };
+    if (childLengthes.length > 0) {
+      const [childLength, ...moreChildLenthes] = childLengthes;
+      item.options = buildTree(childLength, ...moreChildLenthes);
+    }
+    tree.push(item);
+  }
+  return tree;
+};
 
 export default defineComponent({
   components: {
@@ -135,71 +149,14 @@ export default defineComponent({
         value: `options_${char}`,
       };
     });
-    const tree = [{
-      title: '北京市',
-      value: 'beijing',
-      options: [{
-        title: '北京市',
-        value: 'beijing',
-        options: [
-          { title: '东城区', value: 'dongcheng' },
-          { title: '西城区', value: 'xicheng' },
-          { title: '朝阳区', value: 'chaoyang' },
-          { title: '丰台区', value: 'fengtai' },
-          { title: '石景山区', value: 'shijingshan' },
-          { title: '海淀区', value: 'haidian' },
-          { title: '门头沟区', value: 'mentougou' },
-          { title: '房山区', value: 'fangshan' },
-          { title: '通州区', value: 'tongzhou' },
-          { title: '顺义区', value: 'shunyi' },
-        ],
-      }],
-    }, {
-      title: '天津市',
-      value: 'tianjin',
-      options: [{
-        title: '天津市',
-        value: 'tianjin',
-        options: [
-          { title: '和平区', value: 'heping' },
-          { title: '河东区', value: 'hedong' },
-          { title: '河西区', value: 'hexi' },
-        ],
-      }],
-    }, {
-      title: '河北省',
-      value: 'hebei',
-      options: [
-        {
-          title: '石家庄市',
-          value: 'shijiazhuang',
-          options: [
-            { title: '长安区', value: 'changan' },
-            { title: '桥西区', value: 'qiaoxi' },
-            { title: '新华区', value: 'xinhua' },
-            { title: '井陉矿区', value: 'jingjingkuang' },
-            { title: '裕华区', value: 'yuhua' },
-            { title: '藁城区', value: 'gaocheng' },
-          ],
-        },
-        {
-          title: '唐山市',
-          value: 'tangshan',
-          options: [
-            { title: '曹妃甸区', value: 'caofeidian' },
-            { title: '丰南区', value: 'fengnan' },
-            { title: '丰润区', value: 'fengrun' },
-            { title: '古冶区', value: 'guye' },
-            { title: '开平区', value: 'kaiping' },
-            { title: '乐亭区', value: 'laoting' },
-          ],
-        },
-      ],
-    }];
-    const optionsT = ref(tree);
-    const optionsT2 = ref(JSON.parse(JSON.stringify(tree)));
+    const optionsT1 = ref(buildTree(8, 4));
+    const optionsT2 = ref(buildTree(8, 8, 4));
+    const optionsT3 = ref(buildTree(8, 4));
+    const optionsT4 = ref(buildTree(8, 8, 4));
     const treeValue1 = ref(null);
-    const treeValue2 = ref(['tianjin', 'tianjin', ['hedong', 'hexi']]);
+    const treeValue2 = ref(['options_1', 'options_3', ['options_2', 'options_3']]);
+    const treeValue3 = ref(null);
+    const treeValue4 = ref(null);
     const optionsN = ref(numberArr);
     const optionsC = ref(charArr);
     const valueS = ref('option_2');
@@ -208,36 +165,38 @@ export default defineComponent({
     const valueM2 = ref(['options_A', 'options_C']);
 
     return {
-      name,
       optionsN,
       optionsC,
       valueS,
       valueM,
       valueM1,
       valueM2,
-      optionsT,
+      optionsT1,
       optionsT2,
+      optionsT3,
+      optionsT4,
       treeValue1,
       treeValue2,
+      treeValue3,
+      treeValue4,
     };
   },
 });
 </script>
 
 <style lang="less" scoped>
-  .dropdown-menu-wrap{
-    background: #f5f5f5;
-    .t-cell{
-      padding: 0;
+.dropdown-menu-wrap {
+  background: #f5f5f5;
+  .t-cell {
+    padding: 0;
 
-      // background: #f5f5f5;
-      &:not(:last-child){
-        margin-bottom: 16px;
-        &::after{
-          display: none;
-        }
+    // background: #f5f5f5;
+    &:not(:last-child) {
+      margin-bottom: 16px;
+      &::after {
+        display: none;
       }
-
     }
   }
+}
 </style>
