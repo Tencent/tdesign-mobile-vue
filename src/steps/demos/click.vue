f0c225c22e61bfe8ecef32ba9618f80cd13c8114
Petpanda <wangshoumingimis@163.com>
2021-06-09 11:17:57 +0800

    feat: demo改为列表渲染

--- src/steps/demos/click.vue
+++ src/steps/demos/click.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="cell-base">
     <t-steps v-model="current" :readonly="false" class="demo-steps">
-      <t-step title="步骤描述" content="辅助信息文字最多两行"></t-step>
-      <t-step title="选中步骤" content="辅助信息文字最多两行"></t-step>
-      <t-step title="未完成" content="辅助信息文字最多两行"></t-step>
+      <t-step
+        v-for="(item, index) in stepData"
+        :key="index"
+        :title="item.title"
+        :content="item.content"></t-step>
     </t-steps>
   </div>
 </template>
@@ -17,8 +19,23 @@ export default defineComponent({
   name,
   setup() {
     const current = ref(0);
+    const stepData = ref([
+      {
+        title: '步骤描述',
+        content: '辅助信息文字最多两行',
+      },
+      {
+        title: '选中步骤',
+        content: '辅助信息文字最多两行',
+      },
+      {
+        title: '未完成',
+        content: '辅助信息文字最多两行',
+      },
+    ]);
     return {
       current,
+      stepData,
     };
   },
 });
