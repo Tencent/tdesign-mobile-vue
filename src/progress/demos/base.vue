8d5cd7bf2ae76f6b2b7d18d8f40372e9b454f588
JosonYang <258490116@qq.com>
2021-11-28 13:06:15 +0800

    fix: 修正progress的demo&对应API

--- src/progress/demos/base.vue
+++ src/progress/demos/base.vue
@@ -1,46 +1,39 @@
 <template>
-  <div class="cell-base">
-    <t-cell-group title="基础进度条">
-      <t-cell>
-        <t-progress :percentage="0" />
-      </t-cell>
-      <t-cell>
-        <t-progress :percentage="30" />
-      </t-cell>
-       <t-cell>
-         <t-progress :percentage="100" />
-      </t-cell>
-      <t-cell>
-         <t-progress type="error" :percentage="88" />
-      </t-cell>
-    </t-cell-group>
-    <t-cell-group title="隐藏数值进度条">
-      <t-cell>
+  <div class="tdesign-mobile-demo">
+    <h1 class="title">Progress 进度条</h1>
+    <p class="summary">进度展示</p>
+    <tdesign-demo-block title="01 类型" summary="基础进度条">
+      <div class="progress-demo">
         <t-progress :percentage="0" :show-text="false" />
-      </t-cell>
-      <t-cell>
         <t-progress :percentage="30" :show-text="false" />
-      </t-cell>
-      <t-cell>
         <t-progress :percentage="100" :show-text="false" />
-      </t-cell>
-      <t-cell>
-        <t-progress type="error" :percentage="88" :show-text="false" />
-      </t-cell>
-    </t-cell-group>
+      </div>
+    </tdesign-demo-block>
+    <tdesign-demo-block summary="带数值进度条">
+      <div class="progress-demo">
+        <t-progress :percentage="0" />
+        <t-progress :percentage="30" />
+        <t-progress :percentage="100" />
+      </div>
+    </tdesign-demo-block>
+    <tdesign-demo-block title="02 状态" summary="中止状态">
+      <div class="progress-demo">
+        <t-progress type="error" :percentage="50" :show-text="false" />
+        <t-progress type="error" :percentage="50" />
+      </div>
+    </tdesign-demo-block>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import config from '@/config';
-const { prefix } = config;
-const name = `${prefix}-progress-demo`;
-
-export default defineComponent({
-  name,
-  setup() {
-    return {};
-  },
-});
+<script lang="ts" setup>
 </script>
+<style lang="less" scoped>
+.progress-demo{
+  margin-top:16px;
+  padding:16px;
+  background: #fff;
+  .t-progress{
+    margin: 16px 0;
+  }
+}
+</style>
---
+++ src/swiper/props.ts
@@ -0,0 +1,63 @@
+/* eslint-disable */
+
+/**
+ * 该文件为脚本自动生成文件，请勿随意修改。如需修改请联系 PMC
+ * updated at 2021-11-28 12:23:24
+ * */
+
+import { TdSwiperProps } from './type';
+import { PropType } from 'vue';
+
+export default {
+  /** 轮播切换动画效果类型 */
+  animation: {
+    type: String as PropType<TdSwiperProps['animation']>,
+    default: 'slide' as TdSwiperProps['animation'],
+    validator(val: TdSwiperProps['animation']): boolean {
+      return ['slide'].includes(val);
+    },
+  },
+  /** 是否自动播放 */
+  autoplay: {
+    type: Boolean,
+    default: true,
+  },
+  /** 当前轮播在哪一项（下标） */
+  current: {
+    type: Number,
+    default: 0,
+  },
+  /** 当前轮播在哪一项（下标），非受控属性 */
+  defaultCurrent: {
+    type: Number,
+    default: 0,
+  },
+  /** 轮播滑动方向，包括横向滑动和纵向滑动两个方向 */
+  direction: {
+    type: String as PropType<TdSwiperProps['direction']>,
+    default: 'horizontal' as TdSwiperProps['direction'],
+    validator(val: TdSwiperProps['direction']): boolean {
+      return ['horizontal', 'vertical'].includes(val);
+    },
+  },
+  /** 滑动动画时长 */
+  duration: {
+    type: Number,
+    default: 300,
+  },
+  /** 当使用垂直方向滚动时的高度 */
+  height: {
+    type: Number,
+  },
+  /** 轮播间隔时间 */
+  interval: {
+    type: Number,
+    default: 5000,
+  },
+  /** 导航配置。`navigation.type` 表示导航器风格，圆点/分式等，没有值则不显示。`navigation.minShowNum` 表示小于这个数字不会显示导航器。`navigation.showSlideBtn` 表示是否显示两侧的滑动控制按钮 */
+  navigation: {
+    type: Object as PropType<TdSwiperProps['navigation']>,
+  },
+  /** 轮播切换时触发 */
+  onChange: Function as PropType<TdSwiperProps['onChange']>,
+};
