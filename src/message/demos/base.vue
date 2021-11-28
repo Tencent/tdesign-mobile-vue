8075cfdd70ec528a2953d766f332f4f66bd1b4f4
Nice-PLQ <562714485@qq.com>
2021-11-28 15:44:47 +0800

    fix: 优化rate和message的demo展示

--- src/message/demos/base.vue
+++ src/message/demos/base.vue
@@ -1,62 +1,44 @@
 <template>
-  <div class="message-base">
-    <div class="tdesign-demo-block">
-      <t-cell-group title="带图标消息通知">
-        <t-cell value-align="left" class="tdesign-text-message-btn">
-          <t-button size="large" variant="outline" @click="visible = true">普通</t-button>
-          <t-button size="large" variant="outline" @click="visible1 = true">成功</t-button>
-          <t-button size="large" variant="outline" @click="visible2 = true">警示</t-button>
-          <t-button size="large" variant="outline" @click="visible3 = true">错误</t-button>
-        </t-cell>
-      </t-cell-group>
-    </div>
-    <t-cell-group title="纯文本消息">
-      <t-cell value-align="left"  class="tdesign-text-message-btn">
-        <t-button size="large" variant="outline" @click="visible7 = true">普通</t-button>
-        <t-button size="large" variant="outline" @click="visible8 = true">成功</t-button>
-        <t-button size="large" variant="outline" @click="visible9 = true">警示</t-button>
-        <t-button size="large" variant="outline" @click="visible10 = true">错误</t-button>
-      </t-cell>
-    </t-cell-group>
-    <t-cell-group title="自定义配置">
-      <t-cell value-align="left">
-        <t-button size="large" variant="outline" @click="visible5 = true">自定义显示时间</t-button>
-      </t-cell>
-    </t-cell-group>
-    <t-cell-group title="对齐方式">
-      <t-cell value-align="left">
-        <t-button size="large" variant="outline" @click="visible6 = true">居中</t-button>
-      </t-cell>
-    </t-cell-group>
+  <div class="tdesign-mobile-demo">
+    <h1 class="title">Message 消息通知</h1>
+    <p class="summary">用于页面消息通知提示，具有成功提示、提醒、警示等功能，通常在顶部出现，可以自动消失或点击关闭</p>
+    <tdesign-demo-block title="01 类型" summary="弹窗内容为存文本、标题和副标题、带输入框">
+      <div class="button-demo" style="margin: 0 16px">
+        <t-button size="large" variant="outline" @click="visible = true">纯文字通知</t-button>
+        <t-button size="large" variant="outline" @click="visible1 = true">带图标通知</t-button>
+        <t-button size="large" variant="outline" @click="visible2 = true">带关闭通知</t-button>
+        <t-button size="large" variant="outline" @click="visible3 = true">带按钮通知</t-button>
+      </div>
+    </tdesign-demo-block>
 
-    <t-cell-group title="快捷调用">
-      <t-cell value-align="left" class="tdesign-text-message-btn">
-        <t-button size="large" variant="outline" @click="onClick('info')">普通</t-button>
-        <t-button size="large" variant="outline" @click="onClick('success')">成功</t-button>
-        <t-button size="large" variant="outline" @click="onClick('warning')">警示</t-button>
-        <t-button size="large" variant="outline" @click="onClick('error')">错误</t-button>
-      </t-cell>
-    </t-cell-group>
+    <tdesign-demo-block title="02 状态" summary="弹窗内容为存文本、标题和副标题、带输入框">
+      <div class="button-demo" style="margin: 0 16px">
+        <t-button size="large" variant="outline" @click="visible4 = true">普通通知</t-button>
+        <t-button size="large" variant="outline" @click="visible5 = true">成功提示通知</t-button>
+        <t-button size="large" variant="outline" @click="visible6 = true">警示提示通知</t-button>
+        <t-button size="large" variant="outline" @click="visible7 = true">错误提示通知</t-button>
+      </div>
+    </tdesign-demo-block>
 
-    <t-message v-model="visible" content="普通消息"></t-message>
-    <t-message v-model="visible1" content="成功消息" theme="success"></t-message>
-    <t-message v-model="visible2" content="警示消息" theme="warning"></t-message>
-    <t-message v-model="visible3" content="错误消息" theme="error"></t-message>
-    <t-message v-model="visible5" content="5000ms后消失" :duration="5000" />
-    <t-message v-model="visible6" content="居中对齐" align="center" />
-    <t-message v-model="visible7" >纯文本，不带icon</t-message>
-    <t-message v-model="visible8" theme="success">纯文本，不带icon</t-message>
-    <t-message v-model="visible9" theme="warning">纯文本，不带icon</t-message>
-    <t-message v-model="visible10" theme="error">纯文本，不带icon</t-message>
-    <t-message v-model="visible12" >提示文字描述提示文字描述提示文字描述提示文字描述提示文字描述描述描述提示文字描述描描述描述描述提示文字描述</t-message>
+    <t-message v-model="visible">这是一条纯文字的消息通知</t-message>
+    <t-message v-model="visible1" content="这是一条带图标的消息通知"></t-message>
+    <t-message v-model="visible2" content="警示消息"></t-message>
+    <t-message v-model="visible3" content="错误消息"></t-message>
+    <t-message v-model="visible4" content="这是一条普通的通知信息" theme="info"></t-message>
+    <t-message v-model="visible5" content="这是一条需要成功的提示消息" theme="success"></t-message>
+    <t-message v-model="visible6" content="这是一条需要用户关注到的警示通知" theme="warning"></t-message>
+    <t-message v-model="visible7" content="这是一条错误提示通知" theme="error"></t-message>
+
   </div>
 </template>
 
 <script>
 import { defineComponent, ref } from 'vue';
-import Message from '../index';
+import TButton from '../../button';
+import TMessage from '../index';
 
 export default defineComponent({
+  components: { TButton },
   setup() {
     const visible = ref(false);
     const visible1 = ref(false);
@@ -72,7 +54,6 @@ export default defineComponent({
     const visible11 = ref(false);
     const visible12 = ref(false);
 
-
     return {
       visible,
       visible1,
@@ -87,7 +68,7 @@ export default defineComponent({
       visible10,
       visible11,
       visible12,
-      onClick: type => Message[type](type),
+      onClick: (type) => TMessage[type](type),
     };
   },
 });
@@ -99,13 +80,13 @@ export default defineComponent({
 //     margin-right: 20px;
 //   }
 // }
-.tdesign-demo-block{
-  .t-button:not(:last-child){
+.tdesign-demo-block {
+  .t-button:not(:last-child) {
     margin-bottom: 12px;
   }
 }
-.tdesign-text-message-btn{
-  .t-button:not(:last-child){
+.tdesign-text-message-btn {
+  .t-button:not(:last-child) {
     margin-right: 6px;
     margin-bottom: 12px;
   }
--- src/rate/demos/base.vue
+++ src/rate/demos/base.vue
@@ -1,68 +1,73 @@
 <template>
-  <div class="rate-base">
-    <t-cell-group title="评分">
-      <t-cell value-align="left">
+  <div class="tdesign-mobile-demo">
+    <h1 class="title">Rate 按钮</h1>
+    <p class="summary">评分组件，对内容进行快速评级操作，或内容评价的等级展示，目前常用为五星实心评价</p>
+    <tdesign-demo-block title="01 类型" summary="实心评分">
+      <div class="rate-demo-cell">
+        <div class="rate-demo-cell__label">请点击评分</div>
         <t-rate v-model="current" clearable />
-      </t-cell>
-    </t-cell-group>
-
-    <t-cell-group title="允许半选">
-      <t-cell value-align="left">
-        <t-rate v-model="current1" clearable allow-half />
-      </t-cell>
-    </t-cell-group>
-
-    <t-cell-group title="大小">
-      <t-cell value-align="left">
-        <t-rate :value="4" size="32px" />
-      </t-cell>
-    </t-cell-group>
-
-    <t-cell-group title="带辅助文本">
-      <t-cell value-align="left">
-        <t-rate v-model="current2" clearable show-text />
-      </t-cell>
-    </t-cell-group>
-
-    <t-cell-group title="自定义辅助文本">
-      <t-cell value-align="left">
-        <t-rate v-model="current3" clearable show-text :texts="texts" />
-      </t-cell>
-    </t-cell-group>
-
-    <t-cell-group title="自定义数量">
-      <t-cell value-align="left">
-        <t-rate v-model="current4" :count="6" clearable show-text allow-half />
-      </t-cell>
-    </t-cell-group>
-
-    <t-cell-group title="自定义颜色">
-      <t-cell value-align="left">
-        <t-rate v-model="current6" clearable allow-half color="#0052d9" />
-      </t-cell>
-    </t-cell-group>
-
-    <t-cell-group title="自定义图标">
-      <t-cell value-align="left">
-        <t-rate v-model="current5" clearable allow-half>
-          <template #icon>
-            <t-icon name="user-filled" />
-          </template>
-        </t-rate>
-      </t-cell>
-    </t-cell-group>
+      </div>
+    </tdesign-demo-block>
+    <tdesign-demo-block>
+      <p class="tdesign-mobile-demo-block__summary rate">自定义数量评分</p>
+      <div class="rate-demo-cell">
+        <div class="rate-demo-cell__label">自定义数量评分</div>
+        <t-rate v-model="current1" :count="6" clearable />
+      </div>
+    </tdesign-demo-block>
+    <tdesign-demo-block>
+      <p class="tdesign-mobile-demo-block__summary rate">半星评分</p>
+      <div class="rate-demo-cell">
+        <div class="rate-demo-cell__label">半星评分</div>
+        <t-rate v-model="current2" clearable allow-half />
+      </div>
+    </tdesign-demo-block>
+    <tdesign-demo-block>
+      <p class="tdesign-mobile-demo-block__summary rate">带描述评分</p>
+      <div class="rate-demo-cell">
+        <div class="rate-demo-cell__label">请点击评分</div>
+        <t-rate v-model="current3" clearable show-text />
+      </div>
+      <div class="rate-demo-cell">
+        <div class="rate-demo-cell__label">请点击评分</div>
+        <t-rate v-model="current4" clearable show-text :texts="texts" />
+      </div>
+    </tdesign-demo-block>
+    <tdesign-demo-block>
+      <p class="tdesign-mobile-demo-block__summary rate">展示型评分</p>
+      <div style="padding: 12px 16px;">
+        <t-rate v-model="current5" readonly />
+      </div>
+    </tdesign-demo-block>
+    <tdesign-demo-block title="02 规格" summary="评价规格" style="padding-bottom: 16px;">
+      <div class="rate-demo-cell">
+        <div class="rate-demo-cell__label">评分30</div>
+        <t-rate v-model="current5" size="30px" clearable />
+      </div>
+      <div class="rate-demo-cell">
+        <div class="rate-demo-cell__label">评分18</div>
+        <t-rate v-model="current5" size="18px" clearable />
+      </div>
+    </tdesign-demo-block>
   </div>
 </template>
 
 <script>
 import { defineComponent, ref } from 'vue';
+import TCell from '../../cell';
+import TCellGroup from '../../cell-group';
+
 export default defineComponent({
+  components: {
+    TCell,
+    TCellGroup,
+  },
   setup() {
-    const current = ref(0);
-    const current1 = ref(1.5);
-    const current2 = ref(1.5);
+    const current = ref(3);
+    const current1 = ref(3);
+    const current2 = ref(3.5);
     const current3 = ref(3);
-    const current4 = ref(2);
+    const current4 = ref(3);
     const current5 = ref(3);
     const current6 = ref(3.5);
     const texts = ref(['很差', '差', '一般', '好评', '优秀']);
@@ -80,3 +85,21 @@ export default defineComponent({
   },
 });
 </script>
+
+<style lang="less">
+.rate-demo-cell {
+  background-color: #fff;
+  padding: 12px 16px;
+  margin-top: 8px;
+  line-height: 1;
+  display: flex;
+  align-items: center;
+  &__label {
+    font-size: 16px;
+    margin-right: 8px;
+  }
+}
+.tdesign-mobile-demo-block__summary.rate {
+  margin-top: 8px;
+}
+</style>
