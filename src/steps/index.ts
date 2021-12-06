b85ecc3a34fdf98e54a90b3cd5d95542aef67903
JosonYang <258490116@qq.com>
2021-12-06 19:47:15 +0800

    fix: missing WithInstallType

--- src/steps/index.ts
+++ src/steps/index.ts
@@ -5,7 +5,7 @@ import { withInstall, WithInstallType, mapProps } from '../shared';
 import './style';
 
 export const Step: WithInstallType<typeof LocalStep> = withInstall(LocalStep);
-export const Steps: WithInstallType = withInstall(
+export const Steps: WithInstallType<typeof LocalSteps> = withInstall(
   mapProps([
     {
       name: 'current',
