034e8f451b3c1791ac6df02968b8e0030b2ce927
JosonYang <258490116@qq.com>
2021-12-22 00:15:34 +0800

    fix(tab-bar): 调整及对齐api、支持badge

--- src/tab-bar/demos/base.vue
+++ src/tab-bar/demos/base.vue
@@ -11,19 +11,19 @@
     </tdesign-demo-block>
     <tdesign-demo-block summary="文本加图标标签栏">
       <t-tab-bar v-for="(list, index) in demoList_2" :key="index" v-model="value" @change="change">
-        <t-tab-bar-item v-for="(item, i) in list" :key="item.name || i" :name="item.name">
-          <template #icon="props">
-            <img :src="props.isChecked ? icons.active : icons.inactive" alt="" />
+        <t-tab-bar-item v-for="(item, i) in list" :key="item.name || i" :name="item.name" :badge-props="item.badge">
+          <template #icon>
+            <app-icon />
           </template>
           {{ item.text }}
         </t-tab-bar-item>
       </t-tab-bar>
     </tdesign-demo-block>
     <tdesign-demo-block summary="纯图标标签栏">
-      <t-tab-bar v-for="(list, index) in demoList_2" :key="index" v-model="value" @change="change">
+      <t-tab-bar v-for="(list, index) in demoList_3" :key="index" v-model="value" @change="change">
         <t-tab-bar-item v-for="(item, i) in list" :key="item.name || i" :name="item.name">
-          <template #icon="props">
-            <img :src="props.isChecked ? icons.active : icons.inactive" alt="" />
+          <template #icon>
+            <app-icon />
           </template>
         </t-tab-bar-item>
       </t-tab-bar>
@@ -34,7 +34,7 @@
           v-for="(item, index) in list_5"
           :key="item.name || index"
           :name="item.name"
-          :children="item.children"
+          :sub-tab-bar="item.children"
         >
           {{ item.text }}
         </t-tab-bar-item>
@@ -45,9 +45,10 @@
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
-import { ModelValueProps } from '../tab-bar.interface';
+import { AppIcon } from 'tdesign-icons-vue-next';
 
 export default defineComponent({
+  components: { AppIcon },
   setup() {
     const value = ref(null);
     const list_1 = [
@@ -55,11 +56,13 @@ export default defineComponent({
         name: 'label_1',
         text: '标签一',
         children: [],
+        badge: {},
       },
       {
         name: 'label_2',
         text: '标签二',
         children: [],
+        badge: {},
       },
     ];
     const list_2 = [
@@ -68,6 +71,7 @@ export default defineComponent({
         name: 'label_3',
         text: '标签三',
         children: [],
+        badge: {},
       },
     ];
     const list_3 = [
@@ -76,6 +80,7 @@ export default defineComponent({
         name: 'label_4',
         text: '标签四',
         children: [],
+        badge: {},
       },
     ];
     const list_4 = [
@@ -84,6 +89,7 @@ export default defineComponent({
         name: 'label_5',
         text: '标签五',
         children: [],
+        badge: {},
       },
     ];
 
@@ -94,50 +100,74 @@ export default defineComponent({
         text: '此处展开',
         children: [
           {
-            name: 'spread_1',
-            text: '展开项一',
+            value: 'spread_1',
+            label: '展开项一',
           },
           {
-            name: 'spread_2',
-            text: '展开项二',
+            value: 'spread_2',
+            label: '展开项二',
           },
           {
-            name: 'spread_3',
-            text: '展开项三',
+            value: 'spread_3',
+            label: '展开项三',
           },
         ],
       },
     ];
 
-    const change = (changeValue: ModelValueProps) => {
+    const list_6 = [
+      {
+        name: 'label_1',
+        text: '标签一',
+        children: [],
+        badge: {
+          count: '16',
+        },
+      },
+      {
+        name: 'label_2',
+        text: '标签二',
+        children: [],
+        badge: {
+          dot: true,
+        },
+      },
+      {
+        name: 'label_3',
+        text: '标签三',
+        children: [],
+        badge: {
+          count: 'New',
+        },
+      },
+      {
+        name: 'label_4',
+        text: '标签四',
+        children: [],
+        badge: {
+          count: '···',
+        },
+      },
+    ];
+
+    const change = (changeValue: number | string) => {
       console.log('TabBar 值改变为：', changeValue);
     };
 
     return {
       demoList_1: [list_1, list_2, list_3],
-      demoList_2: [list_1, list_2, list_3, list_4],
+      demoList_2: [list_1, list_2, list_3, list_4, list_6],
+      demoList_3: [list_1, list_2, list_3, list_4],
       list_5,
       value,
       change,
-
-      icons: {
-        inactive:
-          'https://imgcache.qq.com/qcloud/tcloud_dtc/static/static_source_business/7bd23b57-07c1-493b-a482-de78f9874a4f.svg',
-        active:
-          'https://imgcache.qq.com/qcloud/tcloud_dtc/static/static_source_business/1097e5e8-a21a-4614-864b-98dec13a0aa5.svg',
-      },
     };
   },
 });
 </script>
 
 <style lang="less" scoped>
-.demo-tab-bar {
-  background-color: #fbfbfb;
-  &__title {
-    padding: 24px 16px 0;
-    font-size: 14px;
-    color: rgba(0, 0, 0, 0.3);
-  }
+.t-tab-bar + .t-tab-bar {
+  margin-top: 16px;
 }
 </style>
