43ee0476f9036038028ace397b5ae48e3740c5dd
Nice-PLQ <562714485@qq.com>
2021-12-24 14:54:59 +0800

    fix: 修复eslint问题

--- src/rate/rate.vue
+++ src/rate/rate.vue
@@ -21,99 +21,19 @@
         </span>
       </li>
     </ul>
-    <span
-      v-if="showText"
-      :style="{
-        color: textColor,
-      }"
-      :class="`${name}--text`"
-      >{{ rateText }}</span
-    >
+    <span v-if="showText" :class="`${name}--text`">{{ rateText }}</span>
   </div>
 </template>
 
 <script lang="ts">
 import { ref, computed, SetupContext, defineComponent, ExtractPropTypes, PropType, ComputedRef } from 'vue';
 import { StarFilledIcon } from 'tdesign-icons-vue-next';
+import rateProps from './props';
 import config from '../config';
 import { emitEvent } from '../shared/emit';
 
 const { prefix } = config;
 const name = `${prefix}-rate`;
-
-const rateProps = {
-  modelValue: Number,
-  /**
-   * @description 选择评分的值
-   * @attribute value
-   */
-  value: Number,
-  /**
-   * @description 评分的数量
-   * @attribute count
-   */
-  count: {
-    type: Number,
-    default: 5,
-  },
-  /**
-   * @description 是否为只读
-   * @attribute readonly
-   */
-  readonly: {
-    type: Boolean,
-    default: false,
-  },
-  /**
-   * @description 是否允许半选
-   * @attribute allow-half
-   */
-  allowHalf: {
-    type: Boolean,
-    default: false,
-  },
-  /**
-   * @description 是否允许取消选择
-   * @attribute clearable
-   */
-  clearable: {
-    type: Boolean,
-    default: false,
-  },
-  /**
-   * @description 评分图标的颜色
-   * @attribute color
-   */
-  color: String,
-  /**
-   * @description 是否显示辅助文字
-   * @attribute show-text
-   */
-  showText: {
-    type: Boolean,
-    default: false,
-  },
-  /**
-   * @description 评分等级对应的辅助文字
-   * @attribute texts
-   */
-  texts: {
-    type: Array as PropType<string[]>,
-    default: (): string[] => [],
-  },
-  /**
-   * @description 评分等级对应的辅助文字颜色
-   * @attribute textColor
-   */
-  textColor: String,
-  /**
-   * @description 评分图标的大小
-   * @attribute size
-   */
-  size: String,
-};
-
-export type RatePropsType = ExtractPropTypes<typeof rateProps>;
 
 interface RangeTypes {
   score: number;
@@ -129,7 +49,7 @@ export default defineComponent({
     const rateWrapper = ref<HTMLElement | null>(null);
     const actualVal = computed(() => props.modelValue || props.value) as ComputedRef<number>;
     const rateText = computed(() => {
-      if (props.texts.length > 0) {
+      if (Array.isArray(props.texts) && props.texts.length > 0) {
         return props.texts[actualVal.value - 1];
       }
 
