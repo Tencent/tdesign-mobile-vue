f805b8f852d1e25932088947358f8c7302da24d2
HQ-Lin <a154571896@gmail.com>
2021-12-28 13:52:17 +0800

    feat: 迁移 md & 优化细节

--- docs/sites/plugin-tdoc/md-to-vue.js
+++ docs/sites/plugin-tdoc/md-to-vue.js
@@ -55,8 +55,11 @@ export default function mdToVue(options) {
           <div v-show="tab === 'api'" name="API">${mdSegment.apiMd}</div>
           <div v-show="tab === 'design'" name="DESIGN">${mdSegment.designMd}</div>
         `
-            : `<div name="DOC">${mdSegment.docMd}</div>`
+            : `<div name="DOC" class="${mdSegment.docClass}">${mdSegment.docMd}</div>`
         }
+        <div style="margin-top: 48px;">
+          <td-doc-history time="${mdSegment.lastUpdated}"></td-doc-history>
+        </div>
         <td-doc-footer slot="doc-footer" platform="mobile"></td-doc-footer>
       </td-doc-content>
     </template>
@@ -139,12 +142,14 @@ function customRender({ source, file, md }) {
     isComponent: false,
     tdDocHeader: true,
     tdDocTabs: DEAULT_TABS,
-    apiFlag: /#+\s*API\n/i,
+    apiFlag: /#+\s*API/i,
+    docClass: '',
+    lastUpdated: Math.round(fs.statSync(file).mtimeMs),
     ...data,
   };
 
   // md filename
-  const reg = file.match(/src\/(\w+-?\w+)\/(\w+-?\w+)\.md/);
+  const reg = file.match(/src\/([\w-]+)\/([\w-]+)\.md/);
   const componentName = reg && reg[1];
 
   // split md
@@ -175,7 +180,7 @@ function customRender({ source, file, md }) {
 
   // 设计指南内容 不展示 design Tab 则不解析
   if (pageData.isComponent && pageData.tdDocTabs.some((item) => item.tab === 'design')) {
-    const designDocPath = path.resolve(__dirname, `../_common/docs/mobile/design/${componentName}.md`);
+    const designDocPath = path.resolve(__dirname, `../../../src/_common/docs/mobile/design/${componentName}.md`);
 
     if (fs.existsSync(designDocPath)) {
       const designMd = fs.readFileSync(designDocPath, 'utf-8');
