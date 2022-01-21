ae965c92597eea357e8afcb5e6192db1870f1897
JosonYang <258490116@qq.com>
2022-01-21 09:45:04 +0800

    fix(swiper): add border radius demo

--- src/swiper/demos/mobile.vue
+++ src/swiper/demos/mobile.vue
@@ -3,41 +3,84 @@
     <h1 class="title">Swiper 轮播</h1>
     <p class="summary">用于循环轮播一组图片或内容，也可以滑动进行切换，轮播动效时间可以设置</p>
     <tdesign-demo-block title="01 类型" summary="轮播图的多种样式">
-      <t-swiper
-        :interval="3000"
-        :autoplay="true"
-        :navigation="{ type: 'dots', showSlideBtn: false }"
-        @change="handleChange"
-      >
-        <t-swiper-item class="swiper-item--demo"> item1 </t-swiper-item>
-        <t-swiper-item style="background: blue" class="swiper-item--demo"> item2 </t-swiper-item>
-        <t-swiper-item style="background: green" class="swiper-item--demo"> item3 </t-swiper-item>
-      </t-swiper>
-      <pagination-type />
+      <div style="padding: 0 16px">
+        <t-swiper
+          :interval="3000"
+          :autoplay="true"
+          :navigation="{ type: 'dots', showSlideBtn: false }"
+          @change="handleChange"
+        >
+          <t-swiper-item class="swiper-item--demo">
+            <img src="https://tdesign.gtimg.com/site/swiper/01.png" class="img" />
+          </t-swiper-item>
+          <t-swiper-item class="swiper-item--demo">
+            <img src="https://tdesign.gtimg.com/site/swiper/02.png" class="img" />
+          </t-swiper-item>
+          <t-swiper-item class="swiper-item--demo">
+            <img src="https://tdesign.gtimg.com/site/swiper/03.png" class="img" />
+          </t-swiper-item>
+        </t-swiper>
 
-      <demo-direction />
+        <t-swiper :autoplay="false" :height="180" direction="vertical" :navigation="{ type: 'fraction' }">
+          <t-swiper-item class="swiper-item--demo1">
+            <img src="https://tdesign.gtimg.com/site/swiper/01.png" class="img" />
+          </t-swiper-item>
+          <t-swiper-item class="swiper-item--demo1">
+            <img src="https://tdesign.gtimg.com/site/swiper/02.png" class="img" />
+          </t-swiper-item>
+          <t-swiper-item class="swiper-item--demo1">
+            <img src="https://tdesign.gtimg.com/site/swiper/03.png" class="img" />
+          </t-swiper-item>
+        </t-swiper>
 
-      <demo-control />
+        <t-swiper :autoplay="false" :navigation="{ type: 'dots-bar' }">
+          <t-swiper-item class="swiper-item--demo">
+            <img src="https://tdesign.gtimg.com/site/swiper/01.png" class="img" />
+          </t-swiper-item>
+          <t-swiper-item class="swiper-item--demo">
+            <img src="https://tdesign.gtimg.com/site/swiper/02.png" class="img" />
+          </t-swiper-item>
+          <t-swiper-item class="swiper-item--demo">
+            <img src="https://tdesign.gtimg.com/site/swiper/03.png" class="img" />
+          </t-swiper-item>
+        </t-swiper>
+
+        <t-swiper
+          v-model:current="state.currentPage"
+          :interval="3000"
+          :autoplay="true"
+          :navigation="{ showSlideBtn: true }"
+        >
+          <t-swiper-item class="swiper-item--demo">
+            <img src="https://tdesign.gtimg.com/site/swiper/01.png" class="img" />
+          </t-swiper-item>
+          <t-swiper-item class="swiper-item--demo">
+            <img src="https://tdesign.gtimg.com/site/swiper/02.png" class="img" />
+          </t-swiper-item>
+          <t-swiper-item class="swiper-item--demo">
+            <img src="https://tdesign.gtimg.com/site/swiper/03.png" class="img" />
+          </t-swiper-item>
+        </t-swiper>
+      </div>
     </tdesign-demo-block>
   </div>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue';
-import PaginationType from './pagination-type.vue';
-import Direction from './direction.vue';
-import Control from './control.vue';
+import { defineComponent, reactive } from 'vue';
 
 export default defineComponent({
-  components: {
-    'pagination-type': PaginationType,
-    'demo-direction': Direction,
-    'demo-control': Control,
-  },
   setup() {
     const handleChange = (index: number) => {
       console.log('基础示例,页数变化到》》》', index);
     };
+    const state: {
+      currentPage: number;
+    } = reactive({
+      currentPage: 1,
+    });
     return {
+      current: 1,
+      state,
       handleChange,
     };
   },
@@ -46,5 +89,24 @@ export default defineComponent({
 <style lang="less">
 .t-swiper {
   margin-bottom: 24px;
+}
+.swiper-item--demo {
+  img {
+    height: 100%;
+  }
+}
+.swiper-item--demo1 {
+  width: 100%;
+  height: 180px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #f33;
+  font-size: 20px;
+  color: #fff;
+
+  img {
+    height: 100%;
+  }
 }
 </style>
