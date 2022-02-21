c384af800f4610fc1ed8d79044d44c865f72f694
xia7187 <xia7187@163.com>
2022-02-21 19:39:06 +0800

    feat: 使用vueSwipe组件优化SwipeCell

--- src/swipe-cell/swipe-cell.vue
+++ src/swipe-cell/swipe-cell.vue
@@ -1,28 +1,16 @@
 <template>
-  <div
-    :class="classes"
-    @touchstart="start"
-    @touchmove="move"
-    @touchend="end"
-    @touchcancel="end"
-    @mousedown="start"
-    @mousemove="move"
-    @mouseup="end"
-  >
+  <div :class="classes" @touchend="end" @touchcancel="end" @mouseup="end">
     <div
       :class="{ [classes + '__wrapper']: true, moving: initData.moving }"
       :style="{ transform: `translate3d(${initData.pos}px,0,0)` }"
     >
-      <!-- 左边按钮 -->
       <div
         ref="leftRef"
         :class="classes + '__left'"
         :style="{ width: initData.leftWidth ? `${initData.leftWidth}px` : 'auto' }"
         @touchstart.stop.passive
       >
-        <!-- 通过插槽插入内容 -->
         <t-node :content="swipeLeftMenu"></t-node>
-        <!-- 通过属性传递的内容 -->
         <template v-for="(btn, index) of left" :key="index">
           <t-button
             :class="btn.className || ''"
@@ -40,20 +28,16 @@
           </t-button>
         </template>
       </div>
-      <!-- 显示内容 -->
       <div ref="swipeCell" class="content">
         <t-node :content="swipeContent"></t-node>
       </div>
-      <!-- 右边按钮 -->
       <div
         ref="rightRef"
         :class="classes + '__right'"
         :style="{ width: initData.rightWidth ? `${initData.rightWidth}px` : 'auto' }"
         @touchstart.stop.passive
       >
-        <!-- 通过插槽插入内容 -->
         <t-node :content="swipeRightMenu"></t-node>
-        <!-- 通过属性传递的内容 -->
         <template v-for="(btn, index) of right" :key="index">
           <t-button
             :class="btn.className || ''"
@@ -87,7 +71,7 @@ import {
   SetupContext,
   getCurrentInstance,
 } from 'vue';
-import { onClickOutside } from '@vueuse/core';
+import { onClickOutside, useSwipe } from '@vueuse/core'; // https://vueuse.org/core/useswipe/
 import config from '../config';
 import { emitEvent } from '../shared/emit';
 import props from './props';
@@ -97,14 +81,6 @@ const { prefix } = config;
 const name = `${prefix}-swipe-cell`;
 
 export interface SwipeInitData {
-  startPoint: {
-    x: number;
-    y: number;
-  };
-  endPoint: {
-    x: number;
-    y: number;
-  };
   moving: boolean;
   leftWidth: number;
   rightWidth: number;
@@ -119,30 +95,52 @@ export default defineComponent({
   emits: ['click', 'change'],
   setup(props, context: SetupContext) {
     const internalInstance = getCurrentInstance();
-    const leftRef = ref<HTMLElement>();
-    const rightRef = ref<HTMLElement>();
-    const swipeCell = ref<HTMLElement>();
     const swipeContent = computed(() => renderContent(internalInstance, 'default', 'content'));
     const swipeLeftMenu = computed(() => renderTNode(internalInstance, 'left'));
     const swipeRightMenu = computed(() => renderTNode(internalInstance, 'right'));
-
-    // distance 滑动多少距离后开始显示菜
-    const distance = 0;
-    // autoBack 点击菜单后是否收回菜
-    const autoBack = true;
-    // threshold 滑动宽度的百分之多少自动打开菜单，否则收回
-    const threshold = 0.5;
-    // 回弹距离
-    const spring = 0;
+    const leftRef = ref<HTMLElement>();
+    const rightRef = ref<HTMLElement>();
+    const swipeCell = ref<HTMLElement>();
+    const { lengthX } = useSwipe(swipeCell, {
+      threshold: 0,
+      onSwipeStart: (e: TouchEvent) => {
+        initData.moving = true;
+        initData.pos = 0;
+      },
+      onSwipe: (e: TouchEvent) => {
+        if (props.disabled) {
+          return;
+        }
+        if (initData.moving && initData.status === 'close') {
+          initData.moving = false;
+        }
+        let pos = 0;
+        if (lengthX.value < 0) {
+          if (initData.leftWidth === 0) {
+            initData.moving = false;
+            return;
+          }
+          pos = Math.min(Math.abs(lengthX.value), initData.leftWidth > 0 ? initData.leftWidth + spring : 0);
+        } else {
+          if (initData.rightWidth === 0) {
+            initData.moving = false;
+            return;
+          }
+          pos = Math.max(-lengthX.value, -(initData.rightWidth > 0 ? initData.rightWidth + spring : 0));
+        }
+        if (initData.status === 'close') {
+          initData.pos = pos;
+        }
+      },
+      onSwipeEnd: (e: TouchEvent) => {
+        end();
+      },
+    });
+    const distance = 0; // distance 滑动多少距离后开始显示菜
+    const autoBack = true; // autoBack 点击菜单后是否收回菜
+    const threshold = 0.5; // threshold 滑动宽度的百分之多少自动打开菜单，否则收回
+    const spring = 0; // 回弹距离
     const initData: SwipeInitData = reactive({
-      startPoint: {
-        x: 0,
-        y: 0,
-      },
-      endPoint: {
-        x: 0,
-        y: 0,
-      },
       moving: false,
       leftWidth: 0, // 左边菜单的宽度
       rightWidth: 0, // 右边菜单的宽度
@@ -151,14 +149,12 @@ export default defineComponent({
     });
     const classes = computed(() => [`${name}`]);
     onMounted(() => {
-      // 计算菜单的宽度
       const leftWidth = leftRef.value?.clientWidth as number;
       const rightWidth = rightRef.value?.clientWidth as number;
       initData.leftWidth = leftWidth > 0 ? leftWidth + distance : 0;
       initData.rightWidth = rightWidth > 0 ? rightWidth + distance : 0;
       renderMenuStatus();
     });
-    // 监听父组件传递的expanded变化
     watch(
       () => props.expanded,
       () => renderMenuStatus(),
@@ -166,7 +162,6 @@ export default defineComponent({
     onClickOutside(swipeCell, (event) => {
       close();
     });
-    // 根据expanded来渲染菜单
     const renderMenuStatus = () => {
       if (props.expanded && props.expanded === 'left') {
         if (initData.leftWidth) {
@@ -179,78 +174,18 @@ export default defineComponent({
         }
       }
     };
-    // 开始滚动
-    const start = (e: any) => {
+    const end = () => {
       if (props.disabled) {
         return;
       }
-      if (e.target.nodeName.toLowerCase() === 'button') {
-        return;
-      }
-      initData.moving = true;
-      if (-initData.pos === initData.rightWidth || initData.pos === initData.leftWidth) {
-        initData.pos = 0;
-      }
-      const { clientX, clientY } = e.touches[0];
-      initData.startPoint = {
-        x: clientX,
-        y: clientY,
-      };
-    };
-    // 正在滚动
-    const move = (e: any) => {
-      if (props.disabled) {
-        return;
-      }
-      if (initData.moving && initData.status === 'close') {
-        initData.moving = false;
-      }
-      // clientX是正在移动的x距离，
-      const { clientX, clientY } = e.changedTouches[0];
-      const { x, y } = initData.startPoint;
-      let pos = clientX - x;
-      if (pos > 0) {
-        if (initData.leftWidth === 0) {
-          initData.moving = false;
-          return;
-        }
-        // 向右滑动，左边按钮出，10是在拉出菜单后，还能拉出的距离，产生一个回弹效果
-        pos = Math.min(pos, initData.leftWidth > 0 ? initData.leftWidth + spring : 0);
-      } else {
-        if (initData.rightWidth === 0) {
-          initData.moving = false;
-        }
-        // 向左滑动，右边按钮出
-        pos = Math.max(pos, -(initData.rightWidth > 0 ? initData.rightWidth + spring : 0));
-      }
-      if (initData.status === 'close') {
-        initData.pos = pos;
-      }
-    };
-    // 滚动结束
-    const end = (e: any) => {
-      if (props.disabled) {
-        return;
-      }
-      const { clientX, clientY } = e.changedTouches[0];
-      initData.endPoint = {
-        x: clientX,
-        y: clientY,
-      };
-      const direction = clientX - initData.startPoint.x > 0 ? 'toRight' : 'toLeft';
-      if (direction === 'toRight') {
-        if (initData.pos > initData.leftWidth * threshold) {
-          open(direction);
-        } else {
-          close('force');
-        }
-      } else if (-initData.pos > initData.rightWidth * threshold) {
-        open(direction);
+      if (lengthX.value < 0 && initData.pos > initData.leftWidth * threshold) {
+        open('toRight');
+      } else if (lengthX.value > 0 && -initData.pos > initData.rightWidth * threshold) {
+        open('toLeft');
       } else {
         close('force');
       }
     };
-    // 开启，通过父组件调用
     const open = (direction: 'toRight' | 'toLeft') => {
       if (initData.status === 'open') {
         return;
@@ -269,7 +204,6 @@ export default defineComponent({
         }
       }
     };
-    // 关闭，可以通过父组件调用
     const close = (type?: string) => {
       if (initData.status === 'close' && type !== 'force') {
         return;
@@ -279,7 +213,6 @@ export default defineComponent({
       initData.pos = 0;
       emitEvent(props, context, 'change', '');
     };
-    // btns按钮点击事件
     const handleClickBtn = ({ action, source }: { action: { [key: string]: any }; source: String }) => {
       emitEvent(props, context, 'click', {
         action,
@@ -291,9 +224,6 @@ export default defineComponent({
     };
     return {
       ...toRefs(props),
-      start,
-      move,
-      end,
       swipeContent,
       swipeLeftMenu,
       swipeRightMenu,
@@ -302,8 +232,7 @@ export default defineComponent({
       swipeCell,
       leftRef,
       rightRef,
-      open,
-      close,
+      end,
       handleClickBtn,
     };
   },
