6bf647c80e2fbd8b4340f0b377a6fdbb50dbc174
JosonYang <258490116@qq.com>
2022-02-22 22:30:12 +0800

    chore: remove segmentcontrol

--- src/segmented-control/index.ts
+++
@@ -1,7 +0,0 @@
-import { withInstall, WithInstallType } from '../shared';
-import SegmentedControl from './segmented-control.vue';
-
-import './style';
-
-const _SegmentedControl: WithInstallType<typeof SegmentedControl> = withInstall(SegmentedControl);
-export default _SegmentedControl;
