19f6b086ba58139c48be56ea48a30febd2c55fc9
HQ-Lin <a154571896@gmail.com>
2022-03-07 18:05:45 +0800

    feat: 调整站点设置

--- site/vite.config.js
+++ site/vite.config.js
@@ -1,40 +1,49 @@
 import * as path from 'path';
 import createTDesignPlugin from './web/plugin-tdoc';
 import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
 
-module.exports = {
-  base: process.env.NODE_ENV === 'production' ? 'https://static.tdesign.tencent.com/mobile-vue/' : '/',
-  root: '.',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, '../src'),
-      'tdesign-moile-vue': path.resolve(__dirname, '../src'),
-      '@common': path.resolve(__dirname, '../src/_common'),
-    },
-  },
-  server: {
-    host: '127.0.0.1',
-    port: 18000,
-    open: '/',
-    https: false,
-  },
-  build: {
-    outDir: '../_site',
-    rollupOptions: {
-      input: {
-        sites: 'index.html',
-        mobile: 'mobile.html',
+const publicPathMap = {
+  preview: '/',
+  intranet: '/mobile-vue/',
+  production: 'https://static.tdesign.tencent.com/mobile-vue/',
+};
+
+export default ({ mode }) => {
+  return defineConfig({
+    base: publicPathMap[mode],
+    root: '.',
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '../src'),
+        'tdesign-moile-vue': path.resolve(__dirname, '../src'),
+        '@common': path.resolve(__dirname, '../src/_common'),
       },
     },
-  },
-  plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: (tag) => tag.startsWith('td-'),
+    server: {
+      host: '127.0.0.1',
+      port: 18000,
+      open: '/',
+      https: false,
+    },
+    build: {
+      outDir: '../_site',
+      rollupOptions: {
+        input: {
+          sites: 'index.html',
+          mobile: 'mobile.html',
         },
       },
-    }),
-    createTDesignPlugin(),
-  ],
+    },
+    plugins: [
+      vue({
+        template: {
+          compilerOptions: {
+            isCustomElement: (tag) => tag.startsWith('td-'),
+          },
+        },
+      }),
+      createTDesignPlugin(),
+    ],
+  });
 };
