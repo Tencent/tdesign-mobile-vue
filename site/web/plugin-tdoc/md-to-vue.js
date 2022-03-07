05108781ab9fc3dd52192cbe30de2fbe832c6dba
HQ-Lin <a154571896@gmail.com>
2022-03-07 16:13:52 +0800

    feat: 调整站点配置

--- site/web/plugin-tdoc/md-to-vue.js
+++ site/web/plugin-tdoc/md-to-vue.js
@@ -177,7 +177,7 @@ function customRender({ source, file, md }) {
   }
 
   // 移动端路由地址
-  const prefix = process.env.NODE_ENV === 'development' ? `/mobile.html` : `/vue-mobile/mobile.html`;
+  const prefix = process.env.NODE_ENV === 'development' ? `/mobile.html` : `/mobile-vue/mobile.html`;
   mdSegment.mobileUrl = `${prefix}#/${componentName}`;
 
   // 设计指南内容 不展示 design Tab 则不解析
