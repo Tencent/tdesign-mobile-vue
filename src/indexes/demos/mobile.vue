44d64375dae0116643251a0a2e68ac93300fd386
ruige24601 <1648555091@qq.com>
2022-05-05 09:30:59 +0800

    feat(indexes): 补充indexes组件的示例

--- src/indexes/demos/mobile.vue
+++ src/indexes/demos/mobile.vue
@@ -1,32 +1,48 @@
 <template>
-  <div class="component-wrap">
-    <t-indexes :list="state.letter" @select="onSelect"> </t-indexes>
+  <div v-if="!showIndexes" class="tdesign-mobile-demo">
+    <h1 class="title">DropdownMenu 下拉菜单</h1>
+    <p class="summary">向下弹出的菜单列表</p>
+    <tdesign-demo-block title="01 类型" summary="单选下拉菜单">
+      <div class="indexes-demo">
+        <t-button size="large" variant="outline" @click="handleClick('letter')"> 字母索引 </t-button>
+        <t-button size="large" variant="outline" @click="handleClick('number')"> 数字索引 </t-button>
+      </div>
+    </tdesign-demo-block>
   </div>
+  <demo-indexes v-else-if="showIndexes" :type="type"></demo-indexes>
 </template>
 <script lang="ts">
+import { toRefs } from '@vueuse/core';
 import { defineComponent, reactive } from 'vue';
-import { letter } from './data';
-import Toast from '../../toast';
+import DemoIndexes from './demo-indexes.vue';
 
 export default defineComponent({
+  components: {
+    DemoIndexes,
+  },
   setup() {
     const state = reactive({
-      letter,
+      showIndexes: false,
+      type: 'letter',
     });
 
-    const onSelect = (indexes: { groupIndex: string; childrenIndex: number }) => {
-      Toast(JSON.stringify(indexes));
+    const handleClick = (type = 'letter') => {
+      state.showIndexes = true;
+      state.type = type;
     };
 
     return {
-      state,
-      onSelect,
+      ...toRefs(state),
+      handleClick,
     };
   },
 });
 </script>
 <style lang="less">
-.component-wrap {
-  height: calc(100vh - 50px);
+.indexes-demo {
+  margin: 0 16px;
+  .t-button {
+    margin-bottom: 16px;
+  }
 }
 </style>
