db717023805ff758c2b26d9ee9abe9f0cb763b21
邓泽玉 <714704978@qq.com>
2022-06-30 15:37:37 +0800

    feat: 删除picker-item的导出

--- src/picker/index.ts
+++ src/picker/index.ts
@@ -1,6 +1,5 @@
 import _Picker from './picker.vue';
 import _Cascade from './cascade.vue';
-import LocalPickerItem from './picker-item.vue';
 import { withInstall, WithInstallType } from '../shared';
 
 import './style';
@@ -11,4 +10,3 @@ export * from './type';
 export type PickerProps = TdPickerProps;
 export const Picker: WithInstallType<typeof _Picker> = withInstall(_Picker);
 export const Cascade: WithInstallType<typeof _Cascade> = withInstall(_Cascade);
-export const PickerItem: WithInstallType<typeof LocalPickerItem> = withInstall(LocalPickerItem);
