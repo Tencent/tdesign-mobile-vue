8b9399031f8f4ff3f30745914d1bc27d965ed633
anlyyao <anly_yaw@163.com>
2022-07-15 16:34:10 +0800

    fix(skeleton): update demo

--- src/skeleton/demos/avatar-text.vue
+++ src/skeleton/demos/avatar-text.vue
@@ -1,4 +1,31 @@
 <template>
-  <t-skeleton theme="avatar-text"></t-skeleton>
+  <div class="group">
+    <t-skeleton class="group-avatar" :row-col="rowColsAvater" :loading="loading"></t-skeleton>
+    <t-skeleton class="group-content" :row-col="rowColsContent" :loading="loading"></t-skeleton>
+  </div>
 </template>
-<script lang="ts"></script>
+<script lang="ts" setup>
+import { toRefs, defineProps } from 'vue';
+
+const props = defineProps({
+  loading: Boolean,
+});
+
+const { loading } = toRefs(props);
+const rowColsAvater = [{ size: '48px', type: 'circle' }];
+const rowColsContent = [{ width: '50%' }, { width: '100%' }];
+</script>
+<style lang="less" scoped>
+.group {
+  display: flex;
+  margin-bottom: 16px;
+}
+
+.group-avatar {
+  margin-right: 12px;
+}
+
+.group-content {
+  width: 100%;
+}
+</style>
