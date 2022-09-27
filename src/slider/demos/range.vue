20a1142212ef6d81926c98995943ef27c6ae7d07
Y <anly_yaw@163.com>
2022-09-27 15:44:37 +0800

    fix(Slider): complete the label attribute && add test unit (#364)

--- src/slider/demos/range.vue
+++ src/slider/demos/range.vue
@@ -1,11 +1,11 @@
 <template>
-  <t-slider v-model="rangeValue" show-value range @change="onChange" />
+  <t-slider v-model="rangeValue" show-extreme-value range @change="onChange" />
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue';
 
-const rangeValue = ref([30, 80]);
+const rangeValue = ref([30, 70]);
 
 function onChange($event: number | number[]) {
   console.log(`change to ${$event}`);
