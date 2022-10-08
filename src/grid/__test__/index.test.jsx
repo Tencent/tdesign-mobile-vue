c983ae65222ad5dc13fc8bfabbacdcf73c346ada
Y <anly_yaw@163.com>
2022-10-08 15:27:57 +0800

    test: improve unit test coverage (#381)

--- src/grid/__test__/index.test.jsx
+++ src/grid/__test__/index.test.jsx
@@ -4,60 +4,151 @@ import Grid from '../grid.vue';
 import GridItem from '../grid-item.vue';
 import Badge from '../../badge/badge.vue';
 
+const prefix = 't'
+const name = `${prefix}-grid`;
 const imgUrl = 'https://tdesign.gtimg.com/mobile/%E5%9B%BE%E7%89%87.png';
+const items = [
+  {
+    text: "标题",
+    image: imgUrl
+  },
+  {
+    text: "标题",
+    image: imgUrl
+  }
+];
+const items2 = [
+  {
+    text: "标题",
+    description: "内容 description",
+    image: imgUrl
+  },
+  {
+    text: "标题",
+    description: "内容 description",
+    image: imgUrl
+  }
+];
+describe('grid', () => {
+  describe('props', () => {
+    it(': align ', async () => {
+      const align = 'left';
+      const wrapper = mount({
+        setup() {
+          return () => (
+            <Grid align={align}>
+              {{
+                default: items.map((item, index) => {
+                  return <GridItem text={item.text + index} image={item.image} />
+                })
+              }}
+            </Grid>
+          )
+        }
+      });
+      expect(wrapper.findComponent(Grid).exists()).toBeTruthy();
+      expect(wrapper.findAllComponents(GridItem).length).toEqual(items.length);
+      const $grid = wrapper.find(`.${name}`);
+      const $gridItems = wrapper.findAll(`.${name}-item`);
+      // align = 'left (自定义)
+      // border = false, column = 4, gutter = 0 (默认)
+      $gridItems.map(item => {
+        expect(item.attributes('class').includes(`${name}-item--bordered`)).toBeFalsy(); // border
+        expect(item.element.style.textAlign).toBe('left') // align
+        expect(item.element.style.flexBasis).toBe(`${100 / 4}%`); // column
+        expect(item.element.style.paddingLeft).toBe(`${0}px`) // gutter
+        expect(item.element.style.paddingRight).toBe(`${0}px`) // gutter
+      })
+    })
 
-describe('Grid.vue', () => {
-  it('create', async () => {
-    const wrapper = mount(() => (
-      <Grid>
-        <GridItem></GridItem>
-        <GridItem></GridItem>
-        <GridItem></GridItem>
-      </Grid>
-    ));
-    expect(wrapper.classes()).toContain('t-grid');
-    expect(wrapper.findAll('.t-grid-item').length).toBe(3);
-  });
+    it(': border ', async () => {
+      const border = {
+        color: 'red',
+        width: '2px',
+        style: 'dashed'
+      };
+      const wrapper = mount({
+        setup() {
+          return () => (
+            <Grid border={border}>
+              {{
+                default: items.map((item, index) => {
+                  return <GridItem text={item.text + index} image={item.image} />
+                })
+              }}
+            </Grid>
+          )
+        }
+      });
+      const $gridItems = wrapper.findAll(`.${name}-item`);
+      // border: object
+      $gridItems.map(item => {
+        expect(item.attributes('class').includes(`${name}-item--bordered`)).toBeTruthy(); // border
+        expect(item.element.style.borderColor).toBe(border.color)
+        expect(item.element.style.borderWidth).toBe(border.width);
+      })
+    })
 
-  it('align render', async () => {
-    const wrapper = mount(() => (
-      <Grid align="left">
-        <GridItem></GridItem>
-        <GridItem></GridItem>
-        <GridItem></GridItem>
-      </Grid>
-    ));
-    const item = wrapper.find('.t-grid-item');
-    expect(item.element.style.textAlign).toBe('left');
-  });
-
-  it('border render', async () => {
-    const wrapper = mount(() => (
-      <Grid border>
-        <GridItem></GridItem>
-        <GridItem></GridItem>
-        <GridItem></GridItem>
-      </Grid>
-    ));
-    const borderItem = wrapper.findAll('.t-grid-item--bordered');
-    expect(borderItem.length).toBe(3);
-  });
-
-  it('column render', async () => {
-    const wrapper = mount(() => (
-      <Grid column={1}>
-        <GridItem></GridItem>
-        <GridItem></GridItem>
-        <GridItem></GridItem>
-      </Grid>
-    ));
-    const item = wrapper.find('.t-grid-item');
-    expect(item.element.style.flexBasis).toBe('100%');
-  });
+    it(': column && gutter ', async () => {
+      const column = 5;
+      const gutter = 10;
+      const wrapper = mount({
+        setup() {
+          return () => (
+            <Grid column={column} gutter={gutter}>
+              {{
+                default: items.map((item, index) => {
+                  return <GridItem text={item.text + index} image={item.image} />
+                })
+              }}
+            </Grid>
+          )
+        }
+      });
+      expect(wrapper.findComponent(Grid).exists()).toBeTruthy();
+      expect(wrapper.findAllComponents(GridItem).length).toEqual(items.length);
+      const $grid = wrapper.find(`.${name}`);
+      const $gridItems = wrapper.findAll(`.${name}-item`);
+      // column = 4, gutter = 10 (自定义)
+      $gridItems.map(item => {
+        expect(item.element.style.textAlign).toBe('center') // align 默认为 center
+        expect(item.element.style.flexBasis).toBe(`${100 / column}%`); // column
+        expect(item.element.style.paddingLeft).toBe(`${gutter}px`) // gutter
+        expect(item.element.style.paddingRight).toBe(`${gutter}px`) // gutter
+      })
+    })
+  })
 });
 
-describe('GridItem.vue', () => {
-  it('badgeProps render', async () => {
+describe('grid-item', () => {
+  describe('props', () => {
+    it(': text && image && description', async () => {
+      const wrapper = mount({
+        setup() {
+          return () => (
+            <Grid>
+              {{
+                default: items2.map((item, index) => {
+                  return <GridItem text={item.text + index} description={item.description + index} image={item.image} />
+                })
+              }}
+            </Grid>
+          )
+        }
+      });
+
+      const $gridItems = wrapper.findAll(`.${name}-item`);
+      $gridItems.map((item, index) => {
+        expect(item.find(`.${name}-item__title`).text()).toEqual(items2[index].text + index);
+        expect(item.find(`.${name}-item__description`).text()).toEqual(items2[index].description + index);
+        const $img = item.find(`.${name}-item__image-box img`);
+        expect($img.exists()).toBeTruthy();
+        expect($img.attributes('src')).toBe(imgUrl);
+      })
+    })
+  })
+
+  it(': badgeProps', async () => {
     const wrapper = mount(() => (
       <Grid>
         <GridItem badgeProps={{ count: 1 }}></GridItem>
@@ -69,43 +160,7 @@ describe('GridItem.vue', () => {
     expect(badge.text()).toBe('1');
   });
 
-  it('text render', async () => {
-    const wrapper = mount(() => (
-      <Grid>
-        <GridItem text="文字信息"></GridItem>
-      </Grid>
-    ));
-    const item = wrapper.find('.t-grid-item');
-    const title = item.find('.t-grid-item__title');
-    expect(title.exists()).toBeTruthy();
-    expect(title.text()).toBe('文字信息');
-  });
-
-  it('description render', async () => {
-    const wrapper = mount(() => (
-      <Grid>
-        <GridItem description="描述信息"></GridItem>
-      </Grid>
-    ));
-    const item = wrapper.find('.t-grid-item');
-    const desc = item.find('.t-grid-item__description');
-    expect(desc.exists()).toBeTruthy();
-    expect(desc.text()).toBe('描述信息');
-  });
-
-  it('image render', async () => {
-    const wrapper = mount(() => (
-      <Grid>
-        <GridItem image={imgUrl}></GridItem>
-      </Grid>
-    ));
-    const item = wrapper.find('.t-grid-item');
-    const img = item.find('.t-grid-item__image-box img');
-    expect(img.exists()).toBeTruthy();
-    expect(img.attributes('src')).toBe(imgUrl);
-  });
-
-  it('layout render', async () => {
+  it(': layout', async () => {
     const wrapper = mount(() => (
       <Grid>
         <GridItem text="标题文字" image={imgUrl} description="描述" layout="horizontal"></GridItem>
