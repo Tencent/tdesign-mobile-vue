593ee5ee254667b587c2ae672a60c3630e5f5cfb
VeryHandSomeBoy <791405318@qq.com>
2022-10-10 10:17:41 +0800

    feat(navbar): update navbar demo

--- src/navbar/demos/mobile.vue
+++ src/navbar/demos/mobile.vue
@@ -3,11 +3,11 @@
     <h1 class="title">NavBar 导航栏</h1>
     <p class="summary">用于不同页面之间切换或者跳转，位于内容区的上方，系统状态栏的下方。</p>
     <tdesign-demo-block title="01 类型" summary="基础导航栏">
-      <t-navbar title="标题" left-icon @left-click="handleClick"> </t-navbar>
+      <t-navbar title="标题" left-icon :fixed="false" @left-click="handleClick"> </t-navbar>
     </tdesign-demo-block>
 
     <tdesign-demo-block summary="左侧双操作导航栏">
-      <t-navbar title="标题">
+      <t-navbar title="标题" :fixed="false">
         <template #left-icon>
           <t-icon size="24px" name="chevron-left" class="btn-left" />
           <t-icon size="24px" name="close" />
@@ -19,7 +19,7 @@
     </tdesign-demo-block>
 
     <tdesign-demo-block summary="右侧双操作导航栏">
-      <t-navbar title="标题" left-icon>
+      <t-navbar title="标题" left-icon :fixed="false">
         <template #right-icon>
           <t-icon size="24px" name="notification" class="btn-left" />
           <t-icon size="24px" name="ellipsis" />
