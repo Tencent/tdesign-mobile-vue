a6b8f9e2fc25df8457628c6851640584aac579cd
anlyyao <anly_yaw@163.com>
2022-10-12 19:21:14 +0800

    feat(Popup): update demo

--- src/popup/demos/placement-top.vue
+++ src/popup/demos/placement-top.vue
@@ -3,7 +3,7 @@
     <div class="button-group">
       <t-button block variant="outline" size="large" @click="top = true">顶部弹出</t-button>
     </div>
-    <t-popup v-model="top" placement="top">
+    <t-popup v-model="top" placement="top" :overlay-props="overlayProps">
       <div style="height: 200px; background: #fff"></div>
     </t-popup>
   </div>
@@ -12,6 +12,9 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
 
+const overlayProps = {
+  customStyle: `background-color: rgba(0,0,0,.3)`, // 自定义遮罩层样式
+};
 const top = ref(false);
 </script>
 
