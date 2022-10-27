20b6cbe6cbbddc45372a2f75ae9dd3088fac3688
zhongyuan <y243139883@163.com>
2022-10-27 16:43:26 +0800

    docs(indexes): update docs and demo

--- src/indexes/demos/mobile.vue
+++ src/indexes/demos/mobile.vue
@@ -1,42 +1,31 @@
 <template>
   <div v-if="!showIndexes" class="tdesign-mobile-demo">
-    <h1 class="title">DropdownMenu 下拉菜单</h1>
-    <p class="summary">向下弹出的菜单列表</p>
-    <tdesign-demo-block title="01 类型" summary="单选下拉菜单">
+    <h1 class="title">Indexes 索引</h1>
+    <p class="summary">用于页面中信息快速检索，可以根据目录中的页码快速找到所需的内容</p>
+    <tdesign-demo-block title="01 类型" summary="基础索引类型">
       <div class="indexes-demo">
         <t-button size="large" variant="outline" block @click="handleClick('letter')"> 字母索引 </t-button>
         <t-button size="large" variant="outline" block @click="handleClick('number')"> 数字索引 </t-button>
       </div>
     </tdesign-demo-block>
   </div>
-  <demo-indexes v-else :type="type"></demo-indexes>
+  <BaseDemo v-else :type="type" @go-back="goBack" />
 </template>
-<script lang="ts">
-import { toRefs } from '@vueuse/core';
-import { defineComponent, reactive } from 'vue';
-import DemoIndexes from './demo-indexes.vue';
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+import BaseDemo from './base.vue';
 
-export default defineComponent({
-  components: {
-    DemoIndexes,
-  },
-  setup() {
-    const state = reactive({
-      showIndexes: false,
-      type: 'letter',
-    });
+const showIndexes = ref(false);
+const type = ref('letter');
+const router = useRouter();
 
-    const handleClick = (type = 'letter') => {
-      state.showIndexes = true;
-      state.type = type;
-    };
+const goBack = () => (showIndexes.value = false);
 
-    return {
-      ...toRefs(state),
-      handleClick,
-    };
-  },
-});
+const handleClick = (t = 'letter') => {
+  showIndexes.value = true;
+  type.value = t;
+};
 </script>
 <style lang="less">
 .indexes-demo {
