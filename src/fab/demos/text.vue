639eb5606609bc27e3a08230d39ba9af7b33adce
zhongyuan <y243139883@163.com>
2022-10-27 15:05:50 +0800

    fix(fab): fix cr

--- src/fab/demos/text.vue
+++ src/fab/demos/text.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
-    <t-fab :icon="iconFunc" text="获取手机号" :button-props="baseButtonProps" :style="customStyle" @click="onClick" />
+    <t-fab :icon="iconFunc" text="按钮文字" :button-props="baseButtonProps" :style="customStyle" @click="onClick" />
   </div>
 </template>
 
 <script lang="ts" setup>
 import { h } from 'vue';
-import { CallIcon } from 'tdesign-icons-vue-next';
+import { AddIcon } from 'tdesign-icons-vue-next';
 
-const iconFunc = () => h(CallIcon);
+const iconFunc = () => h(AddIcon);
 const baseButtonProps = {
   variant: 'outline',
 };
-const customStyle = 'right: 16px; bottom: 96px;';
+const customStyle = 'right: 16px; bottom: 32px;';
 const onClick = () => {
   console.log('click Fab');
 };
