da64548205fc21f9e5589f80fe97c9fda3da7de2
zhongyuan <y243139883@163.com>
2022-10-27 17:33:11 +0800

    docs(sticky): update docs and demo

--- src/sticky/demos/base.vue
+++ src/sticky/demos/base.vue
@@ -1,5 +1,18 @@
 <template>
-  <t-sticky offset-top="50">
-    <t-button>吸顶按钮</t-button>
-  </t-sticky>
+  <div class="block-1">
+    <t-sticky :z-index="50">
+      <t-button theme="primary" class="custom-common-button">基础吸顶</t-button>
+    </t-sticky>
+  </div>
 </template>
+
+<style lang="less" scoped>
+.block-1 {
+  padding-left: 16px;
+}
+.custom-common-button {
+  color: #fff;
+  width: 104px;
+  height: 44px;
+}
+</style>
---
+++ src/sticky/demos/offsetTop.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="block-2">
+    <t-sticky :offset-top="50">
+      <t-button theme="danger" class="custom-common-button">吸顶距离</t-button>
+    </t-sticky>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.block-2 {
+  padding-left: 120px;
+}
+.custom-common-button {
+  color: #fff;
+  width: 104px;
+  height: 44px;
+}
+</style>
