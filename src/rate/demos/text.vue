027a62f21d97737fdaed8452b9aa346ae0d98d6f
zhongyuan <y243139883@163.com>
2022-10-28 17:19:13 +0800

    fix(rate): add space

--- src/rate/demos/text.vue
+++ src/rate/demos/text.vue
@@ -1,11 +1,11 @@
 <template>
-  <CustomCell title="请点击评分">
+  <CustomCell title="请点击评分" class="space">
     <t-rate v-model="value" clearable show-text :gap="defaultGap" :size="16" />
   </CustomCell>
-  <CustomCell title="请点击评分">
+  <CustomCell title="请点击评分" class="space">
     <t-rate v-model="value" clearable show-text :texts="texts" :gap="defaultGap" />
   </CustomCell>
-  <CustomCell title="请点击评分">
+  <CustomCell title="请点击评分" class="space">
     <t-rate v-model="value" clearable show-text :texts="texts" variant="outline" :gap="defaultGap" />
   </CustomCell>
 </template>
@@ -18,3 +18,8 @@ const defaultGap = 8;
 const value = ref(3);
 const texts = ref(['很差', '差', '一般', '好评', '优秀']);
 </script>
+<style lang="less" scoped>
+.space {
+  margin-bottom: 16px;
+}
+</style>
