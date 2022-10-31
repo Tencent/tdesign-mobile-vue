d77fdccd39764a51b98ab006b70bff7ff0e6ddbf
zhongyuan <y243139883@163.com>
2022-10-31 11:56:22 +0800

    fix(rate): fix cr space

--- src/rate/demos/color.vue
+++ src/rate/demos/color.vue
@@ -1,8 +1,8 @@
 <template>
-  <CustomCell title="空心评分">
+  <CustomCell title="空心评分" class="space">
     <t-rate v-model="value1" variant="outline" clearable :color="['#FFC51C', '#E8E8E8']" :gap="defaultGap" />
   </CustomCell>
-  <CustomCell title="实心评分">
+  <CustomCell title="实心评分" class="space">
     <t-rate v-model="value" clearable allow-half :color="['#FFC51C', '#E8E8E8']" :gap="defaultGap" />
   </CustomCell>
 </template>
@@ -15,3 +15,8 @@ const defaultGap = 8;
 const value = ref(2.5);
 const value1 = ref(3);
 </script>
+<style lang="less" scoped>
+.space {
+  margin-bottom: 16px;
+}
+</style>
--- src/rate/demos/size.vue
+++ src/rate/demos/size.vue
@@ -1,8 +1,8 @@
 <template>
-  <CustomCell title="规格20">
+  <CustomCell title="规格20" class="space">
     <t-rate v-model="value1" clearable :gap="defaultGap" />
   </CustomCell>
-  <CustomCell title="规格16">
+  <CustomCell title="规格16" class="space">
     <t-rate v-model="value" size="16" clearable :gap="defaultGap" />
   </CustomCell>
 </template>
@@ -15,3 +15,8 @@ const defaultGap = 8;
 const value = ref(3);
 const value1 = ref(3);
 </script>
+<style lang="less" scoped>
+.space {
+  margin-bottom: 16px;
+}
+</style>
