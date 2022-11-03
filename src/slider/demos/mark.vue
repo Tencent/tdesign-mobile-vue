af0349a46be76dcb60dc12fd555c0ea2da189e09
zhongyuan <y243139883@163.com>
2022-11-03 16:48:03 +0800

    docs(slider): update docs and demo (#458)
    * fix(slider): update docs and demo
    
    * docs(slider): update docs and demo
    
    * fix(slider): update snap

--- src/slider/demos/mark.vue
+++ src/slider/demos/mark.vue
@@ -1,5 +1,5 @@
 <template>
-  <t-slider v-model="value" :label="label" :marks="marks" />
+  <t-slider v-model="value" :label="label" :marks="marks" :step="50" />
 </template>
 
 <script lang="ts" setup>
@@ -8,10 +8,4 @@ import { ref } from 'vue';
 const value = ref(50);
 const label = ref(false);
 const marks = ref({ 0: '小', 50: '中', 100: '大' });
-// const marks = ref([0, 50, 100]);
-// const marks = ref({
-//   0: (val: number) => `${val}%`,
-//   50: (val: number) => `${val}%`,
-//   100: (val: number) => `${val}%`,
-// });
 </script>
