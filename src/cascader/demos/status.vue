dd0591d15649dd409b3df8f51c6e9924389c7005
Shine <791405318@qq.com>
2022-11-03 17:12:50 +0800

    docs: update cascader demo (#451)
    * docs(cascader): update demo code
    
    re #413
    
    * docs(cascader): update cascader demo
    
    re #413

--- src/cascader/demos/status.vue
+++ src/cascader/demos/status.vue
@@ -1,18 +1,11 @@
 <template>
-  <t-input :value="address.address4" label="地址" placeholder="选择地址" readonly @click="show.show4 = true" />
-  <t-popup v-model="show.show4" placement="bottom">
-    <t-cascader
-      v-model="address.address4"
-      :options="provinces"
-      @change="onChange"
-      @cancel="show.show4 = false"
-      @confirm="onConfirm"
-    >
-    </t-cascader>
+  <t-input :value="address" label="地址" placeholder="选择地址" readonly @click="show = true" />
+  <t-popup v-model="show" placement="bottom">
+    <t-cascader v-model="address" :options="provinces" @change="onChange" @cancel="show = false" />
   </t-popup>
 </template>
 <script lang="ts" setup>
-import { reactive } from 'vue';
+import { ref } from 'vue';
 import { TdCascaderItems } from '../type';
 
 const provinces: TdCascaderItems = [
@@ -27,29 +20,11 @@ const provinces: TdCascaderItems = [
   },
 ];
 
-const show = reactive({
-  show1: false,
-  show2: false,
-  show3: false,
-  show4: false,
-});
+const show = ref(false);
+const address = ref('');
 
-const address = reactive({
-  address1: '',
-  address2: '',
-  address3: '',
-  address4: '',
-  address5: '',
-});
-
-const onChange = (value: TdCascaderItems, options: any) => {
-  show.show1 = false;
-  show.show2 = false;
-  show.show3 = false;
-  show.show4 = false;
-};
-
-const onConfirm = () => {
-  console.log('confirm');
+const onChange = (value: string, options: TdCascaderItems) => {
+  address.value = value;
+  show.value = false;
 };
 </script>
