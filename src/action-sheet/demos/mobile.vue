f141484b20b6cb0cf72556d1fb03257c2bbc8279
zhongyuan <y243139883@163.com>
2022-11-07 22:07:33 +0800

    fix(action-sheet): fix cr

--- src/action-sheet/demos/mobile.vue
+++ src/action-sheet/demos/mobile.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="tdesign-mobile-demo" :style="{ 'background-color': '#fff' }">
+  <div class="tdesign-mobile-demo" :style="{ 'background-color': '#fff', height: '100vh' }">
     <h1 class="title">ActionSheet 动作面板</h1>
     <p class="summary">底部弹起的模态面板，包含与当前情境相关的多个选项。</p>
     <tdesign-demo-block title="01 类型" summary="列表型">
