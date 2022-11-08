a6154691b1050d1bf9e41fbf2aa38196ee94b36e
zhongyuan <y243139883@163.com>
2022-11-08 22:47:56 +0800

    fix(pull-down-refresh): fix cr

--- src/pull-down-refresh/demos/mobile.vue
+++ src/pull-down-refresh/demos/mobile.vue
@@ -5,12 +5,8 @@
         <h1 class="title">PullDownRefresh 下拉刷新</h1>
         <p class="summary">用于快速刷新页面信息，刷新可以是整页刷新也可以是页面的局部刷新。</p>
         <div className="tdesign-mobile-block">拖拽该区域演示 顶部下拉刷新</div>
-        <div className="content">
-          <div v-for="i in 1" :key="i" class="row">
-            <div v-for="j in 2" :key="j" class="item">
-              <t-skeleton theme="text" :row-col="rowCols" />
-            </div>
-          </div>
+        <div class="group">
+          <t-skeleton v-for="i in 2" :key="i" :row-col="rowCols" class="item" />
         </div>
       </BaseDemo>
     </tdesign-demo-block>
@@ -23,11 +19,12 @@ import BaseDemo from './base.vue';
 const rowCols = [
   1,
   {
-    width: '80%',
+    width: '100px',
   },
   {
-    height: '171px',
-    borderRadius: '16px',
+    width: '165.5px',
+    height: '165.5px',
+    borderRadius: '12px',
   },
 ];
 </script>
@@ -36,7 +33,8 @@ const rowCols = [
 .tdesign-mobile-block {
   box-sizing: border-box;
   background-color: rgba(243, 243, 243, 1);
-  border-radius: 8px;
+  border-radius: 12px;
+  width: 343px;
   height: 171px;
   font-size: 16px;
   color: rgba(0, 0, 0, 0.26);
@@ -44,17 +42,14 @@ const rowCols = [
   text-align: center;
   margin: 16px;
 }
-.content {
-  box-sizing: border-box;
-  margin: 16px;
-
-  .row {
-    display: flex;
-    justify-content: space-between;
-    .item {
-      width: 47%;
-      margin-bottom: 16px;
-    }
+.group {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  padding: 0 16px 140px 16px;
+  .item {
+    width: 47%;
+    margin-bottom: 16px;
   }
 }
 </style>
