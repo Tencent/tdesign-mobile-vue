8eeada366b9ea3dfa27e1e3ed753ceda025c9eda
zhongyuan <y243139883@163.com>
2022-11-10 11:51:10 +0800

    fix(pull-down-refresh): fix cr

--- src/pull-down-refresh/demos/mobile.vue
+++ src/pull-down-refresh/demos/mobile.vue
@@ -4,7 +4,7 @@
       <BaseDemo>
         <h1 class="title">PullDownRefresh 下拉刷新</h1>
         <p class="summary">用于快速刷新页面信息，刷新可以是整页刷新也可以是页面的局部刷新。</p>
-        <div className="tdesign-mobile-block">拖拽该区域演示 顶部下拉刷新</div>
+        <div class="tdesign-mobile-block">拖拽该区域演示 顶部下拉刷新</div>
         <div class="group">
           <t-skeleton v-for="i in 2" :key="i" :row-col="rowCols" class="item" />
         </div>
@@ -34,7 +34,6 @@ const rowCols = [
   box-sizing: border-box;
   background-color: rgba(243, 243, 243, 1);
   border-radius: 12px;
-  width: 343px;
   height: 171px;
   font-size: 16px;
   color: rgba(0, 0, 0, 0.26);
