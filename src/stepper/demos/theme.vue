e3900b8a7d3d9739caad92ec78b8b0ffc66818b6
anlyyao <anly_yaw@163.com>
2023-03-14 17:49:13 +0800

    feat(Stepper): support size attr

---
+++ src/stepper/demos/theme.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="stepper-example">
+    <t-stepper default-value="3" theme="filled" />
+    <t-stepper default-value="3" theme="outline" />
+    <t-stepper default-value="3" />
+  </div>
+</template>
+
+<style lang="less">
+.stepper-example {
+  padding: 16px;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 32px;
+  background-color: #fff;
+}
+</style>
