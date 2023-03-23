eee4dd9b9ebf6bddf22abfcad1122396cdeb3224
leejimqiu <leejimqiu@gmail.com>
2023-03-23 16:28:18 +0800

    fix(cell): update demo

--- src/cell/demos/group.vue
+++ src/cell/demos/group.vue
@@ -1,12 +1,16 @@
 <template>
   <t-cell-group theme="card">
-    <t-cell :left-icon="h(ServiceIcon)" title="单行标题" arrow />
-    <t-cell :left-icon="h(InternetIcon)" title="单行标题" arrow />
-    <t-cell :left-icon="h(LockOnIcon)" title="单行标题" arrow />
+    <t-cell :left-icon="lock" title="单行标题" arrow />
+    <t-cell :left-icon="service" title="单行标题" arrow />
+    <t-cell :left-icon="internet" title="单行标题" arrow />
   </t-cell-group>
 </template>
 
 <script lang="ts" setup>
 import { h } from 'vue';
 import { LockOnIcon, ServiceIcon, InternetIcon } from 'tdesign-icons-vue-next';
+
+const lock = () => h(LockOnIcon);
+const service = () => h(ServiceIcon);
+const internet = () => h(InternetIcon);
 </script>
