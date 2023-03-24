f69439e7afe21d121d715a791de041a93266450e
leejimqiu <leejimqiu@gmail.com>
2023-03-24 11:21:04 +0800

    refactor(cell): move cell-group folder (#529)
    * refactor(cell): move cell-group folder
    
    * fix(cell): update dom and style
    
    * chore: update common
    
    * fix(cell): update demo
    
    * test(cell): update snapshot

---
+++ src/cell/cell-group.vue
@@ -0,0 +1,27 @@
+<template>
+  <div :class="rootClass">
+    <div v-if="title" :class="`${name}__title`">
+      <slot name="title">{{ title }}</slot>
+    </div>
+
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineProps, computed } from 'vue';
+import CellGroupProps from './cell-group-props';
+import config from '../config';
+
+const { prefix } = config;
+const name = `${prefix}-cell-group`;
+
+export default {
+  name,
+};
+</script>
+
+<script lang="ts" setup>
+const props = defineProps(CellGroupProps);
+const rootClass = computed(() => [name, `${name}--${props.theme}`]);
+</script>
