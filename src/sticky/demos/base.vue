49c472cbb8b9049a3babb153cb551a2af63390f9
Y <anly_yaw@163.com>
2023-04-03 15:07:04 +0800

    feat(Sticky): update demo (#555)

--- src/sticky/demos/base.vue
+++ src/sticky/demos/base.vue
@@ -1,18 +1,11 @@
 <template>
-  <div class="sticky-demo">
-    <t-sticky :z-index="50">
-      <t-button theme="primary" class="custom-common-button">基础吸顶</t-button>
-    </t-sticky>
-  </div>
+  <t-sticky>
+    <t-button theme="primary" size="large" class="common-button">基础吸顶</t-button>
+  </t-sticky>
 </template>
 
 <style lang="less" scoped>
-.sticky-demo {
-  padding-left: 16px;
-}
-.custom-common-button {
-  color: #fff;
+.common-button {
   width: 104px;
-  height: 44px;
 }
 </style>
--- src/sticky/demos/mobile.vue
+++ src/sticky/demos/mobile.vue
@@ -2,11 +2,11 @@
   <div class="tdesign-mobile-demo sticky-demo-container">
     <h1 class="title">Sticky 吸顶</h1>
     <p class="summary">用于常驻页面顶部的信息、操作展示。</p>
-    <tdesign-demo-block summary="基础吸顶">
+    <tdesign-demo-block summary="基础吸顶" padding>
       <BaseDemo />
     </tdesign-demo-block>
     <tdesign-demo-block summary="吸顶距离">
-      <OffsetTopDemo />
+      <OffsetDemo />
     </tdesign-demo-block>
     <tdesign-demo-block summary="指定容器">
       <ContainerDemo />
@@ -16,7 +16,7 @@
 
 <script setup lang="ts">
 import BaseDemo from './base.vue';
-import OffsetTopDemo from './offsetTop.vue';
+import OffsetDemo from './offset.vue';
 import ContainerDemo from './container.vue';
 </script>
 
