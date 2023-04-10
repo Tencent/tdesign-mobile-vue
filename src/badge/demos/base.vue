f1c1c07b001fe107ec599c754643e8685a887e72
jarmywang <awangjianjun9@gmail.com>
2023-04-11 07:40:38 +0800

    refactor(badge): demo base dot

--- src/badge/demos/base.vue
+++ src/badge/demos/base.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="tdesign-mobile-demo">
+    <div class="summary">红点徽标</div>
     <div class="badge-demo">
-      <div class="badge-item">
-        <t-badge dot>消息</t-badge>
-      </div>
-      <div class="badge-item">
-        <t-badge count="16">消息</t-badge>
-      </div>
-      <div class="badge-item">
-        <t-badge count="NEW">消息</t-badge>
-      </div>
-      <div class="badge-item">
-        <t-badge count="···">消息</t-badge>
-      </div>
+      <t-badge dot :offset="[-4, 4]" class="badge-item" content="消息" />
+      <t-badge dot :offset="[1, -1]" class="badge-item">
+        <t-icon name="notification" size="24" ariaLabel="通知" />
+      </t-badge>
+      <t-badge dot :offset="[1, 1]" class="badge-item">
+        <t-button>按钮</t-button>
+      </t-badge>
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import { Icon as TIcon } from 'tdesign-icons-vue-next';
+</script>
--- src/badge/demos/mobile.vue
+++ src/badge/demos/mobile.vue
@@ -2,7 +2,7 @@
   <div class="tdesign-mobile-demo">
     <h1 class="title">Badge 徽标</h1>
     <p class="summary">用于告知用户，该区域的状态变化或者待处理任务的数量。</p>
-    <tdesign-demo-block title="01 类型" summary="徽标主要分红点、数字、文字和角标提醒">
+    <tdesign-demo-block title="01 组件类型">
       <BaseDemo />
     </tdesign-demo-block>
     <tdesign-demo-block>
@@ -19,10 +19,3 @@ import BaseDemo from './base.vue';
 import ButtonDemo from './button.vue';
 import CellDemo from './cell.vue';
 </script>
-
-<style>
-.tdesign-mobile-demo-block .badge-demo .badge-item {
-  width: 24%;
-  text-align: center;
-}
-</style>
