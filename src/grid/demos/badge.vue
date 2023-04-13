268f971e349f170273f7e5b2ed0ea2117446a91e
leejimqiu <leejimqiu@gmail.com>
2023-04-13 16:49:41 +0800

    fix: update demo

--- src/grid/demos/badge.vue
+++ src/grid/demos/badge.vue
@@ -1,9 +1,9 @@
 <template>
   <t-grid class="grid-demo">
-    <t-grid-item text="标题文字" :image="imgUrl" :badge-props="{ dot: true }" />
-    <t-grid-item text="标题文字" :image="imgUrl" :badge-props="{ count: 8 }" />
-    <t-grid-item text="标题五字内" :image="imgUrl" :badge-props="{ count: 13 }" />
-    <t-grid-item text="标题五字内" :image="imgUrl" :badge-props="{ count: 'NEW' }" />
+    <t-grid-item text="标题文字" :image="imgUrl" :badge="{ dot: true }" />
+    <t-grid-item text="标题文字" :image="imgUrl" :badge="{ count: 8 }" />
+    <t-grid-item text="标题五字内" :image="imgUrl" :badge="{ count: 13 }" />
+    <t-grid-item text="标题五字内" :image="imgUrl" :badge="{ count: 'NEW' }" />
   </t-grid>
 </template>
 
