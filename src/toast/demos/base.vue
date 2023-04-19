4435424fa92e27db9baccd3b72d6b6e27832a76f
thewang <thewang@tencent.com>
2023-04-19 11:36:41 +0800

    feat: 修改说明文档

---
+++ src/toast/demos/base.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="toast-demo">
+    <t-button block size="large" theme="primary" variant="outline" @click="showText()">纯文本</t-button>
+    <t-button block size="large" theme="primary" variant="outline" @click="showMultiText()">多行文字</t-button>
+    <t-button block size="large" theme="primary" variant="outline" @click="showHorizontalText()">带横向图标</t-button>
+    <t-button block size="large" theme="primary" variant="outline" @click="showVerticalText()">带竖向图标</t-button>
+    <t-button block size="large" theme="primary" variant="outline" @click="showLoading()">加载状态</t-button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { h, ref } from 'vue';
+import { Toast } from 'tdesign-mobile-vue';
+
+const showText = () => {
+  Toast('轻提示文字内容');
+};
+const showMultiText = () => {
+  Toast('最多一行展示十个汉字宽度限制最多不超过三行文字行文字行文字');
+};
+const showHorizontalText = () => {
+  Toast({
+    theme: 'success',
+    direction: 'row',
+    message: '轻提示文字内容',
+  });
+};
+const showVerticalText = () => {
+  Toast({
+    theme: 'success',
+    direction: 'column',
+    message: '轻提示文字内容',
+  });
+};
+const showLoading = () => {
+  Toast({
+    theme: 'loading',
+    message: '轻提示文字内容',
+  });
+};
+</script>
