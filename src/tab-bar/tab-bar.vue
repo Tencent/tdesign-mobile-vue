bf5c3a5869e2ec638a7bb2a8fc4c0fb36c0f5e68
jarmywang <awangjianjun9@gmail.com>
2023-04-28 20:43:33 +0800

    refactor(tabBar): finish (#633)

--- src/tab-bar/tab-bar.vue
+++ src/tab-bar/tab-bar.vue
@@ -1,14 +1,14 @@
 <template>
-  <div :class="tabbarClass" role="tablist">
+  <div :class="tabBarClass" role="tablist">
     <slot />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, provide, Ref, computed } from 'vue';
+import { defineComponent, ref, provide, Ref, computed, toRefs, onMounted } from 'vue';
 import TabBarProps from './props';
 import config from '../config';
-import { useDefault } from '../shared';
+import { useDefault, useChildSlots } from '@/shared';
 
 const { prefix } = config;
 const name = `${prefix}-tab-bar`;
@@ -20,27 +20,40 @@ export default defineComponent({
   setup(props, context) {
     const [activeValue] = useDefault(props, context.emit, 'value', 'change');
     const defaultIndex: Ref<number> = ref(-1);
+    const itemCount = ref(0);
+
+    onMounted(() => {
+      const nodes = context.slots.default && context.slots.default();
+      if (nodes !== undefined) {
+        const childSlots = useChildSlots(`${prefix}-tab-bar-item`);
+        itemCount.value = childSlots.length;
+      }
+    });
 
     const updateChild = (currentValue: number | string) => {
       activeValue.value = currentValue;
     };
 
-    const tabbarClass = computed(() => [
+    const tabBarClass = computed(() => [
       `${name}`,
       {
         [`${name}--bordered`]: props.bordered,
         [`${name}--fixed`]: props.fixed,
+        [`${name}--safe`]: props.safeAreaInsetBottom,
       },
+      `${name}--${props.shape}`,
     ]);
 
     provide('tab-bar', {
+      ...toRefs(props),
       defaultIndex,
       activeValue,
+      itemCount,
       updateChild,
     });
 
     return {
-      tabbarClass,
+      tabBarClass,
     };
   },
 });
