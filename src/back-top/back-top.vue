1973608ae6497bba10530addfbfa18662564d07f
nined9 <359080688@qq.com>
2023-04-30 21:26:53 +0800

    feat: use template and update snap

--- src/back-top/back-top.vue
+++ src/back-top/back-top.vue
@@ -1,3 +1,10 @@
+<template>
+  <div :class="classes" @click="clickBackBtn">
+    <t-node :content="iconTNode" />
+    <span v-if="text" :class="`${name}__text--${theme}`">{{ text }}</span>
+  </div>
+</template>
+
 <script lang="ts">
 import { computed, defineComponent, getCurrentInstance, h } from 'vue';
 import { useElementBounding } from '@vueuse/core';
@@ -11,7 +18,7 @@ const { prefix } = config;
 const name = `${prefix}-back-top`;
 export default defineComponent({
   name,
-  components: { TNode, TIconBackTop },
+  components: { TNode },
   props: BackTopProps,
   setup(props, context) {
     const emitEvent = useEmitEvent(props, context.emit);
@@ -28,39 +35,25 @@ export default defineComponent({
     });
 
     const internalInstance = getCurrentInstance();
+    const backTopIcon = h(TIconBackTop, { size: '22px' });
     const iconTNode = computed(() => {
       if (context.slots?.icon || typeof props.icon === 'function') {
         return renderTNode(internalInstance, 'icon');
       }
-      return h(TIconBackTop, { size: '22px' });
+      return backTopIcon;
     });
 
     const clickBackBtn = () => {
       window.document.documentElement.scrollTop += top.value;
       emitEvent('to-top');
     };
-    return () => {
-      return h(
-        'div',
-        {
-          class: classes.value,
-          onClick: clickBackBtn,
-        },
-        [
-          h(TNode, { content: iconTNode.value }),
-          ...(props.text
-            ? [
-                h(
-                  'span',
-                  {
-                    class: `${name}__text--${props.theme}`,
-                  },
-                  props.text,
-                ),
-              ]
-            : []),
-        ],
-      );
+    return {
+      name,
+      classes,
+      iconTNode,
+      el,
+      top,
+      clickBackBtn,
     };
   },
 });
