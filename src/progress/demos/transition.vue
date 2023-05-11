45f340560744c651a127bd07c22c663280971a17
Y <anly_yaw@163.com>
2023-05-11 10:37:19 +0800

    refactor(progress): new progress (#640)
    * refactor(progress): new progress
    
    * chore: update _common

---
+++ src/progress/demos/transition.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="example-progress">
+    <t-progress :percentage="percentage" />
+    <div class="button-group">
+      <t-button theme="primary" variant="outline" size="small" @click="clickReduce">减少</t-button>
+      <div class="space"></div>
+      <t-button theme="primary" size="small" @click="clickAdd">增加</t-button>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const percentage = ref(88);
+
+function clickAdd() {
+  percentage.value = Math.min(100, percentage.value + 10);
+}
+function clickReduce() {
+  percentage.value = Math.max(0, percentage.value - 10);
+}
+</script>
+<style lang="less" scoped>
+.example-progress {
+  .button-group {
+    display: flex;
+    justify-content: center;
+    padding-bottom: 8px;
+    .space {
+      width: 16px;
+    }
+  }
+}
+</style>
