fc2fff915df5791ebe39de7b1085410aa48a183e
Nined <359080688@qq.com>
2023-05-19 10:53:37 +0800

    refactor(SwipeCell): new SwipeCell (#663)
    * refactor(swipe-cell): new swipe-cell
    
    * feat: add sure confirm
    
    * feat: update docs and optimize sliding
    
    * fix: spell
    
    * feat: update new docs and optimize sliding area

---
+++ src/shared/useClickAway/index.ts
@@ -0,0 +1,98 @@
+import { MaybeElementRef, UnRefElementReturn, unrefElement, useEventListener } from '@vueuse/core';
+import { isArray } from 'lodash';
+import { inBrowser } from '../util';
+
+export interface UseClickAwayOptions {
+  /**
+   * The name of the trigger event.
+   * @default 'touchstart'
+   */
+  eventName?: string;
+  /**
+   * Use capturing phase for internal event listener.
+   * @default true
+   */
+  capture?: boolean;
+  /**
+   * List of elements that should not trigger the event.
+   */
+  ignore?: Array<MaybeElementRef>;
+  /**
+   * Run handler function if focus moves to an iframe.
+   * @default false
+   */
+  detectIframe?: boolean;
+}
+
+export type UseClickAwayHandler<
+  T extends { detectIframe: UseClickAwayOptions['detectIframe'] } = { detectIframe: false },
+> = (evt: T['detectIframe'] extends true ? PointerEvent | FocusEvent : PointerEvent) => void;
+
+/**
+ * Listen for clicks outside of a collection of elements.
+ * @param target
+ * @param fn
+ * @param options
+ */
+export function useClickAway<T extends UseClickAwayOptions>(
+  target: MaybeElementRef | Array<MaybeElementRef>,
+  fn: UseClickAwayHandler<{ detectIframe: T['detectIframe'] }>,
+  options: T = {} as T,
+) {
+  if (!inBrowser) return;
+
+  const { eventName = 'touchstart', capture = true, ignore = [], detectIframe = false } = options;
+
+  const listenerOptions = {
+    passive: true,
+    capture,
+  };
+
+  // el是否包含在event.target
+  const checkElInEvtTarget = (el: UnRefElementReturn, event: PointerEvent) => {
+    return el && (event.target === el || event.composedPath().includes(el));
+  };
+
+  const shouldIgnore = (event: PointerEvent) => {
+    return ignore.some((ignoreTarget) => checkElInEvtTarget(unrefElement(ignoreTarget), event));
+  };
+
+  const eventHandler = (event: PointerEvent) => {
+    const targets = isArray(target) ? [...target] : [target];
+
+    let shouldTrigger = targets.every((targetItem) => !checkElInEvtTarget(unrefElement(targetItem), event));
+
+    if (!shouldTrigger) return;
+
+    shouldTrigger = !shouldIgnore(event);
+
+    if (!shouldTrigger) return;
+
+    fn(event);
+  };
+
+  const cleanup = [useEventListener(window, eventName, eventHandler, listenerOptions)];
+
+  if (detectIframe) {
+    cleanup.push(
+      useEventListener(window, 'blur', (event: PointerEvent) => {
+        setTimeout(() => {
+          const targets = isArray(target) ? [...target] : [target];
+
+          if (
+            targets.every((targetItem) => {
+              const el = unrefElement(targetItem);
+              return (
+                window.document.activeElement?.tagName === 'IFRAME' && !el?.contains(window.document.activeElement)
+              );
+            })
+          ) {
+            fn(event);
+          }
+        }, 0);
+      }),
+    );
+  }
+
+  return () => cleanup.forEach((clean) => clean());
+}
---
+++ src/swipe-cell/demos/left.vue
@@ -0,0 +1,91 @@
+<template>
+  <tdesign-demo-block title="组件类型" summary="左滑动操作">
+    <t-swipe-cell ref="cell1" opened>
+      <t-cell title="左滑单操作" note="辅助信息"></t-cell>
+      <template #right>
+        <div class="btn delete-btn" @click="handleClick">删除</div>
+      </template>
+    </t-swipe-cell>
+  </tdesign-demo-block>
+  <tdesign-demo-block style="margin-top: 10px">
+    <t-swipe-cell ref="cell2" opened>
+      <t-cell title="左滑单操作" note="辅助信息" description="一段很长很长的内容文字"></t-cell>
+      <template #right>
+        <div class="btn delete-btn" @click="handleClick">删除</div>
+      </template>
+    </t-swipe-cell>
+  </tdesign-demo-block>
+  <tdesign-demo-block style="margin-top: 10px">
+    <t-swipe-cell opened :right="initData.btns.slice(1)" @click="(value: any) => handleClickBtns(value)">
+      <t-cell title="左滑双操作" note="辅助信息"></t-cell>
+    </t-swipe-cell>
+  </tdesign-demo-block>
+  <tdesign-demo-block style="margin-top: 10px">
+    <t-swipe-cell :right="initData.btns" opened @click="(value:any) => handleClickBtns(value)">
+      <t-cell title="左滑三操作" note="辅助信息"></t-cell>
+    </t-swipe-cell>
+  </tdesign-demo-block>
+</template>
+
+<script setup lang="ts">
+import { reactive } from 'vue';
+import Toast from '../../toast/index';
+import { SwipeActionItem } from '../type';
+
+interface InitData {
+  btns: {}[];
+}
+const handleCollect = () => {
+  Toast.success(`收藏成功`);
+};
+const handleEdit = () => {
+  Toast.success(`编辑成功`);
+};
+
+const handleDelete = () => {
+  Toast.success(`删除成功`);
+};
+
+const handleClickBtns = (value: { action: SwipeActionItem; source: String }) => {
+  Toast(JSON.stringify(value));
+};
+
+const initData: InitData = reactive({
+  btns: [
+    { text: '收藏', className: 'btn favor-btn', onClick: handleCollect },
+    { text: '编辑', className: 'btn edit-btn', onClick: handleEdit },
+    { text: '删除', className: 'btn delete-btn', onClick: handleDelete },
+  ],
+});
+const handleClick = (e: Event) => {
+  Toast('click');
+};
+</script>
+
+<style lang="less">
+.btn-wrapper {
+  height: 100%;
+}
+
+.btn {
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  padding: 0 16px;
+  color: white;
+  font-size: 14px;
+}
+
+.delete-btn {
+  background-color: #e34d59;
+}
+
+.edit-btn {
+  background-color: #ed7b2f;
+}
+
+.favor-btn {
+  background-color: #0052d9;
+}
+</style>
