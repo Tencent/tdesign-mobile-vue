d1e870e9fcc3e5f4476678cf7442da15e2309b31
anlyyao <anly_yaw@163.com>
2023-06-13 18:26:43 +0800

    fix: import reset.css for website

--- site/mobile/main.ts
+++ site/mobile/main.ts
@@ -5,6 +5,7 @@ import DemoBlock from './components/demo-block.vue';
 import router from './router';
 
 import TDesign from '@/index';
+import '@common/style/mobile/_reset.less';
 import '../styles/mobile/index.less';
 
 createApp(app)
