df6b92bda04999ebc9dafcf31f58582a43698c94
Y <anly_yaw@163.com>
2023-06-14 15:17:41 +0800

    fix(Loading): resolved style error for demo (#866)
    * fix(Loading): resolved style error for demo
    
    * test: update snapshots

--- src/loading/demos/mobile.vue
+++ src/loading/demos/mobile.vue
@@ -2,35 +2,31 @@
   <div class="tdesign-mobile-demo">
     <h1 class="title">Loading 加载中</h1>
     <p class="summary">用于表示页面或操作的加载状态，给予用户反馈的同时减缓等待的焦虑感，由一个或一组反馈动效组成。</p>
-    <tdesign-demo-block title="01 组件类型" summary="纯图标">
-      <div class="loading-demo first">
+    <tdesign-demo-block title="01 组件类型" summary="纯图标" :padding="true">
+      <div class="loading-demo--flex">
         <base-vue />
       </div>
     </tdesign-demo-block>
-    <tdesign-demo-block summary="图标加文字横向">
-      <div class="loading-demo">
+    <tdesign-demo-block summary="图标加文字横向" :padding="true">
+      <div class="loading-demo--flex">
         <horz />
       </div>
     </tdesign-demo-block>
-    <tdesign-demo-block summary="图标加文字竖向">
-      <div class="loading-demo">
+    <tdesign-demo-block summary="图标加文字竖向" :padding="true">
+      <div class="loading-demo--flex">
         <vert />
       </div>
     </tdesign-demo-block>
-    <tdesign-demo-block summary="纯文字">
-      <div class="loading-demo">
+    <tdesign-demo-block summary="纯文字" :padding="true">
+      <div class="loading-demo--flex">
         <pure-text />
       </div>
     </tdesign-demo-block>
     <tdesign-demo-block title="02 组件尺寸">
-      <div class="loading-demo loading-demo--column">
-        <size />
-      </div>
+      <size />
     </tdesign-demo-block>
     <tdesign-demo-block title="03 加载速度" summary="加载速度调整">
-      <div class="loading-demo loading-demo--column">
-        <speed />
-      </div>
+      <speed />
     </tdesign-demo-block>
   </div>
 </template>
@@ -45,28 +41,8 @@ import size from './size.vue';
 </script>
 
 <style scoped lang="less">
-.loading-demo {
+.loading-demo--flex {
   display: flex;
   align-items: center;
-  padding: 0 16px;
-
-  &--column {
-    flex-direction: column;
-    align-items: flex-start;
-    .t-loading {
-      margin-bottom: 12px;
-    }
-  }
-
-  &.first,
-  &.second {
-    .t-loading {
-      margin-right: 36px;
-    }
-
-    .dots-loading {
-      margin-left: 14px;
-    }
-  }
 }
 </style>
--- src/loading/demos/size.vue
+++ src/loading/demos/size.vue
@@ -1,16 +1,36 @@
 <template>
-  <div class="tdesign-mobile-demo-block__summary">大尺寸</div>
-  <t-loading size="26px" text="加载中..." />
-  <div class="tdesign-mobile-demo-block__summary">中尺寸</div>
-  <t-loading size="22px" text="加载中..." />
-  <div class="tdesign-mobile-demo-block__summary">小尺寸</div>
-  <t-loading size="20px" text="加载中..." />
+  <div class="loading-demo">
+    <div class="loading-demo__summary mb-16">大尺寸</div>
+    <t-loading size="26px" text="加载中..." />
+  </div>
+  <div class="loading-demo">
+    <div class="loading-demo__summary mt-24 mb-16">中尺寸</div>
+    <t-loading size="22px" text="加载中..." />
+  </div>
+  <div class="loading-demo">
+    <div class="loading-demo__summary mt-24 mb-16">小尺寸</div>
+    <t-loading size="20px" text="加载中..." />
+  </div>
 </template>
 
 <script lang="ts" setup></script>
 
 <style lang="less" scoped>
-.tdesign-mobile-demo-block__summary {
-  padding-left: 0;
+.loading-demo {
+  padding: 0 16px;
+}
+
+.loading-demo__summary {
+  font-size: 14px;
+  color: rgba(0, 0, 0, 0.6);
+  // margin: 8px 16px 0;
+  line-height: 22px;
+}
+.mt-24 {
+  margin-top: 24px;
+}
+
+.mb-16 {
+  margin-bottom: 16px;
 }
 </style>
