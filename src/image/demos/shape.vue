b5742b8db0b48ecbc0d40d5abe0279677550c273
Y <anly_yaw@163.com>
2023-06-16 17:57:36 +0800

    fix(Image): resolved props.error default value invalid (#885)
    * fix(Image): fix demo
    
    * fix(Image): resolved props.error default value invalid
    
    * fix(Image): update demo
    
    * test: update snapshots
    
    * fix: fix cr

--- src/image/demos/shape.vue
+++ src/image/demos/shape.vue
@@ -1,16 +1,22 @@
 <template>
-  <div class="image-group">
-    <div class="image-demo">
-      <p class="image-demo-tip">方形</p>
-      <t-image class="image-container" :src="imageSrc" />
+  <div class="image-example">
+    <div class="image-example-title">不同形状的图片</div>
+    <div class="image-example-desc">
+      提供方形、圆角方形、圆角 3 种形状。 当图片长宽不相等时，无法使用 circle 展示一个正圆。
     </div>
-    <div class="image-demo">
-      <p class="image-demo-tip">圆角方形</p>
-      <t-image class="image-container" :src="imageSrc" shape="round" />
-    </div>
-    <div class="image-demo">
-      <p class="image-demo-tip">圆形</p>
-      <t-image class="image-container" alt="一张图片" :src="imageSrc" fit="cover" shape="circle" />
+    <div class="image-group">
+      <div class="image-demo">
+        <p class="image-demo-tip">方形</p>
+        <t-image class="image-container" :src="imageSrc" />
+      </div>
+      <div class="image-demo">
+        <p class="image-demo-tip">圆角方形</p>
+        <t-image class="image-container" :src="imageSrc" shape="round" />
+      </div>
+      <div class="image-demo">
+        <p class="image-demo-tip">圆形</p>
+        <t-image class="image-container" alt="一张图片" :src="imageSrc" fit="cover" shape="circle" />
+      </div>
     </div>
   </div>
 </template>
@@ -18,29 +24,42 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
 
-const imageSrc = ref('https://tdesign.gtimg.com/site/upload1.png');
+const imageSrc = ref('https://tdesign.gtimg.com/demo/demo-image-1.png');
 </script>
 
 <style lang="less" scoped>
+.image-example {
+  margin-bottom: 24px;
+  color: rgba(0, 0, 0, 0.6);
+
+  &-title {
+    font-size: 14px;
+    line-height: 22px;
+  }
+
+  &-desc {
+    font-size: 12px;
+    line-height: 20px;
+  }
+}
+
 .image-group {
-  margin-top: 24px;
   display: flex;
-  padding: 0 16px;
   flex-wrap: wrap;
 
   .image-demo {
-    width: 72px;
     margin-right: 24px;
-
     .image-container {
-      width: 100%;
       height: 72px;
+      width: 72px;
+      background-color: #000;
     }
+
     .image-demo-tip {
       color: rgba(0, 0, 0, 0.6);
       font-size: 14px;
       line-height: 22px;
-      margin-bottom: 16px;
+      margin: 16px 0;
       white-space: nowrap;
     }
   }
