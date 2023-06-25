89e919c2c56880b9c6c06f2974d240ac248492fc
leejimqiu <leejimqiu@gmail.com>
2023-06-25 15:20:23 +0800

    refactor(fab): using setup

--- src/fab/fab.vue
+++ src/fab/fab.vue
@@ -1,46 +1,45 @@
 <template>
   <div :class="classes" :style="style" @click="onClick">
-    <t-button size="large" theme="primary" :class="[`${name}__button`]" v-bind="btnProps" :icon="() => iconTNode">
+    <t-button
+      size="large"
+      theme="primary"
+      :shape="props.text ? 'round' : 'circle'"
+      :class="[`${name}__button`]"
+      v-bind="buttonProps"
+      :icon="() => iconTNode"
+    >
       {{ text }}
     </t-button>
   </div>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, getCurrentInstance } from 'vue';
+import { computed, defineProps, defineEmits, getCurrentInstance } from 'vue';
 
 import { renderTNode, useEmitEvent } from '../shared';
-import props from './props';
+import fabProps from './props';
 import config from '../config';
-import TButton, { ButtonProps } from '../button';
+import TButton from '../button';
 
 const { prefix } = config;
 const name = `${prefix}-fab`;
 
-export default defineComponent({
+export default {
   name,
-  components: { TButton },
-  props,
-  emits: ['click'],
-  setup(props, context) {
-    const internalInstance = getCurrentInstance();
-    const iconTNode = computed(() => renderTNode(internalInstance, 'icon'));
-    const emitEvent = useEmitEvent(props, context.emit);
+};
+</script>
 
-    const classes = computed(() => ({
-      [`${name}`]: true,
-    }));
-    const btnProps = computed<ButtonProps>(() => ({ shape: props.text ? 'round' : 'circle', ...props.buttonProps }));
+<script lang="ts" setup>
+const props = defineProps(fabProps);
+const emit = defineEmits(['click']);
 
-    const onClick = (e: MouseEvent) => emitEvent('click', { e });
+const internalInstance = getCurrentInstance();
+const iconTNode = computed(() => renderTNode(internalInstance, 'icon'));
+const emitEvent = useEmitEvent(props, emit);
 
-    return {
-      name,
-      classes,
-      btnProps,
-      iconTNode,
-      onClick,
-    };
-  },
-});
+const classes = computed(() => ({
+  [`${name}`]: true,
+}));
+
+const onClick = (e: MouseEvent) => emitEvent('click', { e });
 </script>
