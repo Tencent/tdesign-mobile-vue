d875bc95fa16ebf9bf95de4bde4de11046964de5
anlyyao <anly_yaw@163.com>
2023-07-06 16:41:42 +0800

    fix: resolved home page

--- site/mobile/components/home.vue
+++ site/mobile/components/home.vue
@@ -4,9 +4,17 @@
       <img src="https://tdesign.gtimg.com/site/TDesign.png" alt="logo" />
       <p>TDesign 适配 Vue 的移动端组件库</p>
     </div>
-    <t-collapse v-for="doc in docs.filter((x) => x.type === 'component')" :key="doc.title">
-      <t-collapse-panel :name="doc.title" :title="doc.title">
-        <t-cell-group>
+    <t-collapse
+      v-for="(doc, index) in docs.filter((x) => x.type === 'component')"
+      :key="doc.title"
+      :expand-icon="false"
+    >
+      <t-collapse-panel
+        :value="index"
+        :header="doc.title"
+        :expand-icon="() => h(iconDefault[doc.titleEn], { size: '24px' })"
+      >
+        <t-cell-group class="collapse__content">
           <t-cell
             v-for="comItem in doc.children.filter((x) => x.name !== 'icon')"
             :key="comItem.name"
@@ -18,20 +26,32 @@
         </t-cell-group>
       </t-collapse-panel>
     </t-collapse>
-    <!-- <div class="tdesign-demo-home-navs" v-for="(navItem, index) in navs" :key="index">
-      <router-link class="tdesign-demo-home-navlink" :to="`${navItem.name}`">
-        {{ navItem.title }}
-      </router-link>
-    </div> -->
   </div>
 </template>
 <script lang="ts" setup>
-import TIcon from 'tdesign-icons-vue-next';
-import siteConfig from '../../docs.config';
+import { h } from 'vue';
+import { AppIcon, BulletpointIcon, ViewModuleIcon, ImageIcon, ChatIcon } from 'tdesign-icons-vue-next';
+import { docs } from '../../docs.config';
 import router from '../router';
 
-const { docs } = siteConfig;
+const iconDefault = {
+  Base: AppIcon,
+  Navigation: ViewModuleIcon,
+  Form: BulletpointIcon,
+  'Data Display': ImageIcon,
+  FeedBack: ChatIcon,
+};
+
 const onClick = (comItem: any) => {
   router.push({ path: comItem.name });
 };
 </script>
+
+<style lang="less" scoped>
+.tdesign-mobile-home {
+  .collapse__content {
+    --td-cell-horizontal-padding: 0;
+    --td-cell-right-icon-font-size: 16px;
+  }
+}
+</style>
