3b0e4325384446b7dde68135c2c9fb9fdc913fbf
anlyyao <anly_yaw@163.com>
2023-07-13 14:46:17 +0800

    test: test case

--- src/button/demos/mobile.vue
+++ src/button/demos/mobile.vue
@@ -2,7 +2,16 @@
   <div class="tdesign-mobile-demo">
     <h1 class="title">Button 按钮</h1>
     <p class="summary">按钮用于开启一个闭环的操作任务，如“删除”对象、“购买”商品等。</p>
-    <tdesign-demo-block title="01 组件类型" summary="基础按钮">
+    <div class="row">
+      点击禁用
+
+      <t-button size="large" :disabled="status" theme="primary" @click="status = true">动态禁用</t-button>
+    </div>
+    <div>
+      默认禁用
+      <t-button size="large" disabled theme="primary">默认禁用</t-button>
+    </div>
+    <!-- <tdesign-demo-block title="01 组件类型" summary="基础按钮">
       <BaseButtonDemo />
     </tdesign-demo-block>
 
@@ -36,20 +45,24 @@
 
     <tdesign-demo-block summary="按钮主题">
       <themeDemo />
-    </tdesign-demo-block>
+    </tdesign-demo-block> -->
   </div>
 </template>
 <script lang="ts" setup>
-import './style/index.less';
-import BaseButtonDemo from './base.vue';
-import iconDemo from './icon.vue';
-import ghostDemo from './ghost.vue';
-import groupDemo from './group.vue';
-import blockDemo from './block.vue';
-import statusDemo from './status.vue';
-import sizeDemo from './size.vue';
-import shapeDemo from './shape.vue';
-import themeDemo from './theme.vue';
+// import './style/index.less';
+// import BaseButtonDemo from './base.vue';
+// import iconDemo from './icon.vue';
+// import ghostDemo from './ghost.vue';
+// import groupDemo from './group.vue';
+// import blockDemo from './block.vue';
+// import statusDemo from './status.vue';
+// import sizeDemo from './size.vue';
+// import shapeDemo from './shape.vue';
+// import themeDemo from './theme.vue';
+
+import { ref } from 'vue';
+
+const status = ref(false);
 </script>
 
 <style scoped>
