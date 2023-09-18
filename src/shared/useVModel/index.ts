1671db03fe167e19566dc16aa5c5e753571aa304
bitjian <1183048003@qq.com>
2023-09-18 12:29:20 +0800

    fix: fix(cascader): 修复变更选项触发两次onchange事件和v-model绑定ref()没有响应式问题
    fix(cascader): 修复变更选项触发两次onchange事件和v-model绑定ref()没有响应式问题

--- src/shared/useVModel/index.ts
+++ src/shared/useVModel/index.ts
@@ -1,5 +1,4 @@
 import { ref, Ref, getCurrentInstance, ComponentInternalInstance } from 'vue';
-import kebabCase from 'lodash/kebabCase';
 
 export type ChangeHandler<T> = (value: T, ...args: any[]) => void;
 
@@ -11,20 +10,12 @@ export function useVModel<T>(
   propName = 'value',
   // emit 和 eventName 用于支持 v-model 和 xxx.sync 语法糖
 ): [Ref<T>, ChangeHandler<T>] {
-  const { emit, vnode } = getCurrentInstance() as ComponentInternalInstance;
+  const { emit } = getCurrentInstance() as ComponentInternalInstance;
   const internalValue = ref<T>() as Ref<T>;
   internalValue.value = defaultValue;
 
-  const vProps = vnode.props || {};
-  const isVM =
-    Object.prototype.hasOwnProperty.call(vProps, 'modelValue') ||
-    Object.prototype.hasOwnProperty.call(vProps, 'model-value');
-  const isVMP =
-    Object.prototype.hasOwnProperty.call(vProps, propName) ||
-    Object.prototype.hasOwnProperty.call(vProps, kebabCase(propName));
-
   // 受控模式 v-model:propName
-  if (isVMP) {
+  if (typeof value.value !== 'undefined') {
     return [
       value,
       (newValue, ...args) => {
@@ -35,7 +26,7 @@ export function useVModel<T>(
   }
 
   // 受控模式:modelValue v-model
-  if (isVM) {
+  if (typeof modelValue.value !== 'undefined') {
     return [
       modelValue,
       (newValue, ...args) => {
