ae476f8f15d402e718ebe38eab909ea1d66f7a21
Y <anly_yaw@163.com>
2024-03-21 15:56:28 +0800

    Revert "fix(tag): 修复tag不更新 (#1265)" (#1271)
    This reverts commit 2a781340f7554aca432c6659378ecf457f1e1a95.

--- src/tag/tag.vue
+++ src/tag/tag.vue
@@ -1,13 +1,10 @@
 <template>
   <span :class="classes" :style="tagStyle" :aria-disabled="disabled" role="button" @click="handleClick">
     <span :class="`${baseClass}__icon`">
-      <slot name="icon"></slot>
+      <t-node :content="iconContent"></t-node>
     </span>
     <span :class="`${baseClass}__text`">
-      <slot v-if="slotDefault"></slot>
-      <slot v-else name="content">
-        {{ content }}
-      </slot>
+      <t-node :content="tagContent"></t-node>
     </span>
     <span v-if="closable" :class="`${baseClass}__icon-close`" @click.stop="onClickClose">
       <close-icon />
@@ -17,9 +14,10 @@
 
 <script lang="ts">
 import { CloseIcon } from 'tdesign-icons-vue-next';
-import { defineComponent, computed, useSlots, toRefs } from 'vue';
+import { defineComponent, computed, getCurrentInstance } from 'vue';
 import config from '../config';
 import TagProps from './props';
+import { renderContent, renderTNode, TNode } from '../shared';
 
 const { prefix } = config;
 const name = `${prefix}-tag`;
@@ -28,11 +26,14 @@ const Tag = defineComponent({
   name,
   components: {
     CloseIcon,
+    TNode,
   },
   props: TagProps,
   emits: ['close', 'click'],
   setup(props) {
-    const slotDefault = !!useSlots().default;
+    const internalInstance = getCurrentInstance();
+    const tagContent = computed(() => renderContent(internalInstance, 'default', 'content'));
+    const iconContent = computed(() => renderTNode(internalInstance, 'icon'));
     const baseClass = name;
 
     const tagStyle = computed(() => {
@@ -66,13 +67,13 @@ const Tag = defineComponent({
     };
 
     return {
-      ...toRefs(props),
       baseClass,
       classes,
       tagStyle,
       onClickClose,
       handleClick,
-      slotDefault,
+      iconContent,
+      tagContent,
     };
   },
 });
