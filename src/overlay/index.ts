19e7fb318cf91f0155144cdda1a1a2ad54a527b6
黎伟杰 <674416404@qq.com>
2024-04-24 12:19:23 +0800

    fix: ts type export

--- src/overlay/index.ts
+++ src/overlay/index.ts
@@ -1,7 +1,10 @@
-import { withInstall, WithInstallType } from '../shared';
 import _Overlay from './overlay';
+import { withInstall, WithInstallType } from '../shared';
 
+import { TdOverlayProps } from './type';
 import './style';
+
+export type OverlayProps = TdOverlayProps;
 
 const Overlay: WithInstallType<typeof _Overlay> = withInstall(_Overlay);
 export default Overlay;
