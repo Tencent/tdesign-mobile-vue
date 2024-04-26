37f4be4fc077ae0e589dc13f2685802e8a789153
zelmazhou <532773417@qq.com>
2024-04-26 18:37:12 +0800

    feat: swiper-item完成tsx

--- src/swiper/index.ts
+++ src/swiper/index.ts
@@ -1,5 +1,5 @@
 import _Swiper from './swiper.vue';
-import _SwiperItem from './swiper-item.vue';
+import _SwiperItem from './swiper-item';
 import { withInstall, WithInstallType } from '../shared';
 import { TdSwiperProps } from './type';
 
---
+++ src/swiper/swiper-item.tsx
@@ -0,0 +1,63 @@
+import { ref, inject, computed, getCurrentInstance, onMounted, onUnmounted, watch, defineComponent } from 'vue';
+import { useElementBounding } from '@vueuse/core';
+import { usePrefixClass } from '../hooks/useClass';
+import { useTNodeJSX } from '../hooks/tnode';
+
+import config from '../config';
+
+const { prefix } = config;
+const name = `${prefix}-swiper-item`;
+export default defineComponent({
+  name,
+  setup() {
+    const { addChild, removeChild, isVertical, root, loop, items, setContainerHeight } = inject('parent') as any;
+    const swiperItemClass = usePrefixClass('swiper-item');
+    const readerTNodeJSX = useTNodeJSX();
+    const selfRef = ref(null);
+    const rootStyle = ref('');
+    const instance = getCurrentInstance();
+
+    const direction = computed(() => (isVertical.value ? 'Y' : 'X'));
+
+    const calcTranslateStyle = (index: number, activeIndex: number) => {
+      const distance = root.value?.[isVertical.value ? 'offsetHeight' : 'offsetWidth'] ?? 0;
+      const lastItemIndex = items.value.length - 1;
+      let step = index - activeIndex;
+      // lastItem
+      if (activeIndex === lastItemIndex && index === 0) {
+        step = 1;
+      }
+      // firstItem
+      if (activeIndex === 0 && index === lastItemIndex && index !== 1) {
+        step = -1;
+      }
+
+      if (activeIndex === index) step = 0;
+
+      rootStyle.value = `transform: translate${direction.value}(${step * distance}px)`;
+    };
+    const { height } = useElementBounding(selfRef);
+
+    watch(height, (val) => {
+      setContainerHeight(val);
+    });
+
+    onMounted(() => {
+      addChild({
+        proxy: instance?.proxy,
+        uid: instance?.uid,
+        calcTranslateStyle,
+      });
+    });
+
+    onUnmounted(() => {
+      removeChild(instance?.uid);
+    });
+
+    return () => (
+      <div ref={selfRef} class={swiperItemClass.value} style={rootStyle.value}>
+        {readerTNodeJSX('default')}
+      </div>
+    );
+  },
+});
