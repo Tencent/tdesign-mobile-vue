c777ca07a1675e3b726755b381223cfde21dcaed
Dexter <52393227+dexterBo@users.noreply.github.com>
2024-06-11 19:47:02 +0800

    feat(loading): loading新增attach、fullscreen属性，支持函数式调用 (#1444)
    * feat(loading): loading新增attach、fullscreen属性，支持函数式调用
    
    * feat(loading): 组件示例更新
    
    * fix: fix lint
    
    * chore(Loading): add loadingPlugin to plugins file
    
    ---------
    
    Co-authored-by: anlyyao <anly_yaw@163.com>

---
+++ src/loading/demos/fullscreen.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="loading-demo">
+    <t-loading :loading="loading" text="加载中..." fullscreen />
+    <div>
+      全局加载开关（开启加载1秒后自动归位）：
+      <t-switch v-model="loading" size="small"></t-switch>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+import { Switch as TSwitch } from 'tdesign-mobile-vue';
+
+const loading = ref(false);
+
+watch(
+  () => loading.value,
+  (v) => {
+    if (!v) return;
+    const timer = setTimeout(() => {
+      loading.value = false;
+      clearTimeout(timer);
+    }, 1000);
+  },
+);
+</script>
+
+<style lang="less" scoped>
+.loading-demo {
+  padding: 0 16px;
+}
+</style>
