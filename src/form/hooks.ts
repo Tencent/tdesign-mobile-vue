1437f66618fcb6c20462e3bb99d68846a4549689
黎伟杰 <674416404@qq.com>
2024-06-26 15:54:16 +0800

    fix: cr

--- src/form/hooks.ts
+++ src/form/hooks.ts
@@ -8,26 +8,13 @@ export interface FormDisabledProvider {
 
 /**
  * 用于实现 form 的全局禁用状态hook
- * 禁用优先级: 组件 > 组件组 > 表单(checkbox.disabled > checkboxGroup.disabled > form.disabled)
+ * 禁用优先级: 组件 > 组件组 > 表单(propsDisabled.value > extend?.value > disabled?.value)
  * @returns
  */
 export function useFormDisabled(extend?: Ref<boolean>) {
   const ctx = getCurrentInstance();
   const propsDisabled = computed(() => ctx?.props.disabled as boolean);
   const { disabled } = inject<FormDisabledProvider>('formDisabled', Object.create(null));
-  return computed(() => {
-    // 组件
-    if (isBoolean(propsDisabled.value)) {
-      return propsDisabled.value;
-    }
-    // 组件组
-    if (isBoolean(extend?.value)) {
-      return extend.value;
-    }
-    // 表单
-    if (isBoolean(disabled?.value)) {
-      return disabled.value;
-    }
-    return false;
-  });
+
+  return computed(() => propsDisabled.value || extend?.value || disabled?.value || false);
 }
