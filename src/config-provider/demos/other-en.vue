c5d5d2dee3e5bfa24be232791682f5fc9e41341f
Y. <anly_yaw@163.com>
2024-09-13 15:33:27 +0800

    chore: add alias configuration (#1587)

--- src/config-provider/demos/other-en.vue
+++ src/config-provider/demos/other-en.vue
@@ -32,7 +32,7 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
 import merge from 'lodash/merge';
-import enConfig from '../../locale/en_US';
+import enConfig from 'tdesign-mobile-vue/es/locale/en_US';
 
 // 全局特性配置，可以引入英文默认配置 enConfig，还可以在默认配置的基础上进行自定义配置
 const globalConfig = merge(enConfig, {
