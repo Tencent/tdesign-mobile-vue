5e4d0cf47b83d1b3118d1de9acd015a044084106
黎伟杰 <674416404@qq.com>
2025-01-25 18:16:17 +0800

    chore: fix site

--- site/web/plugin-tdoc/md-to-vue.js
+++ site/web/plugin-tdoc/md-to-vue.js
@@ -2,7 +2,7 @@
 import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
-import camelcase from 'camelcase';
+import camelCase from 'camelcase';
 
 import testCoverage from '../test-coverage';
 
