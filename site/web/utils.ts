fed51bcd9eba910b42520a37a617edcc46f2ff24
anlyyao <anly_yaw@163.com>
2025-03-13 17:36:45 +0800

    fix(site): fix version number parsing error

--- site/web/utils.ts
+++ site/web/utils.ts
@@ -26,17 +26,38 @@ export function sortDocs(docs: DocType[]): DocType[] {
   return innerDocs;
 }
 
-// 过滤小版本号
-export function filterVersions(versions = []) {
-  const versionMap = new Map();
+function compareVersion(version1, version2) {
+  const v1 = version1.split('.');
+  const v2 = version2.split('.');
+  const len = Math.max(v1.length, v2.length);
 
-  versions.forEach((v: string) => {
-    if (v.includes('-')) return false;
-    const nums = v.split('.');
-    versionMap.set(`${nums[0]}.${nums[1]}`, v);
-  });
+  while (v1.length < len) {
+    v1.push('0');
+  }
+  while (v2.length < len) {
+    v2.push('0');
+  }
 
-  return [...versionMap.values()].sort(
-    (a, b) => Number(a.split('.').slice(0, 2).join('.')) - Number(b.split('.').slice(0, 2).join('.')),
-  );
+  for (let i = 0; i < len; i++) {
+    const num1 = parseInt(v1[i], 10);
+    const num2 = parseInt(v2[i], 10);
+
+    if (num1 > num2) {
+      return 1;
+    }
+    if (num1 < num2) {
+      return -1;
+    }
+  }
+
+  return 0;
+}
+
+function filterStableVersions(versionList) {
+  return versionList.filter((version) => !version.includes('-'));
+}
+
+// 过滤小版本 + 版本号排序
+export function filterVersions(versions) {
+  return filterStableVersions(versions).sort(compareVersion);
 }
