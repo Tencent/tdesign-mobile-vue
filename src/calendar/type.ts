481df88f86bf503d9aa56a491ad532329c6a0c9e
黎伟杰 <674416404@qq.com>
2025-06-04 23:40:43 +0800

    chore: reset change

--- src/calendar/type.ts
+++ src/calendar/type.ts
@@ -14,11 +14,6 @@ export interface TdCalendarProps {
    */
   confirmBtn?: string | ButtonProps | TNode | null;
   /**
-   * 是否显示日历；`usePopup` 为 true 时有效。非受控属性
-   * @default false
-   */
-  defaultVisible?: boolean;
-  /**
    * 第一天从星期几开始，默认 0 = 周日
    * @default 0
    */
