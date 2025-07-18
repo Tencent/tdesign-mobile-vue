123529c6533e9d05f7962aab5ec35f28d77289b8
Wesley <985189328@qq.com>
2025-07-18 14:57:10 +0800

    feat: add QRCode component (#1921)
    * feat: add QRCode
    
    * chore: slot
    
    * chore: optimize
    
    * chore: optimize
    
    * chore: optimize
    
    * chore(demo): rename the example file names using kebab-case
    
    * chore: remove useless files
    
    * test: generate coverage
    
    * fix: fix test
    
    ---------
    
    Co-authored-by: anlyyao <anly_yaw@163.com>

---
+++ src/qrcode/demos/base.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="tdesign-demos-qrcode">
+    <t-qrcode value="https://tdesign.tencent.com/" />
+    <t-qrcode value="https://tdesign.tencent.com/" borderless />
+  </div>
+</template>
