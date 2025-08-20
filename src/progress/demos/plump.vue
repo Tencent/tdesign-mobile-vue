5f91691291cff2c3d28f73e533635eabfdb35f33
anlyyao <anly_yaw@163.com>
2025-08-20 20:05:34 +0800

    chore(Progress): update demo

--- src/progress/demos/plump.vue
+++ src/progress/demos/plump.vue
@@ -4,13 +4,16 @@
       <t-progress theme="plump" :percentage="80" />
     </div>
     <div class="example-progress__item">
-      <t-progress theme="plump" :percentage="88" status="warning" />
+      <t-progress theme="plump" :percentage="100" />
     </div>
     <div class="example-progress__item">
-      <t-progress theme="plump" :percentage="88" status="error" />
+      <t-progress theme="plump" :percentage="80" status="warning" />
     </div>
     <div class="example-progress__item">
-      <t-progress theme="plump" :percentage="88" status="success" />
+      <t-progress theme="plump" :percentage="80" status="error" />
+    </div>
+    <div class="example-progress__item">
+      <t-progress theme="plump" :percentage="80" :color="['#0052D9', '#2BA471']" />
     </div>
   </div>
 </template>
