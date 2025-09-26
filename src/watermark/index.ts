536db3d5beb68f89e4c6b377fa17710826363fb9
Wesley <985189328@qq.com>
2025-09-26 16:22:24 +0800

    feat: add watermark

---
+++ src/watermark/index.ts
@@ -0,0 +1,7 @@
+import { withInstall } from '@tdesign/shared-utils';
+import _Watermark from './watermark';
+
+export * from './type';
+
+export const Watermark = withInstall(_Watermark);
+export default Watermark;
