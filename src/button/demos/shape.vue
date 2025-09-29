9f2dc7270cb79b9b8799583818c9a4b33d827af6
slatejack <1815846446@qq.com>
2025-09-29 17:40:14 +0800

    fix(button): fix button demo style

--- src/button/demos/shape.vue
+++ src/button/demos/shape.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="row">
     <t-button theme="primary" size="large">填充按钮</t-button>
-    <t-button theme="primary" size="large" :icon="searchIcon" shape="square" aria-label="搜索"></t-button>
+    <t-button theme="primary" size="large" :icon="appIcon" shape="square" aria-label="搜索"></t-button>
     <t-button theme="primary" size="large" shape="round">填充按钮</t-button>
-    <t-button theme="primary" size="large" :icon="searchIcon" shape="circle" aria-label="搜索"></t-button>
+    <t-button theme="primary" size="large" :icon="appIcon" shape="circle" aria-label="搜索"></t-button>
   </div>
 
   <t-button theme="primary" size="large" block class="rectangle-button">填充按钮</t-button>
@@ -11,9 +11,9 @@
 
 <script lang="ts" setup>
 import { h } from 'vue';
-import { SearchIcon } from 'tdesign-icons-vue-next';
+import { AppIcon } from 'tdesign-icons-vue-next';
 
-const searchIcon = () => h(SearchIcon, { size: '24px' });
+const appIcon = () => h(AppIcon, { size: '24px' });
 </script>
 
 <style scoped>
