033ac7096fa00b6dc03aa47aa10c041cec44f40b
mikasayw <mikasayw@163.com>
2025-10-23 23:32:39 +0800

    fix(calendar): 修复value存在时日历定位不准确问题

--- src/calendar/calendar.tsx
+++ src/calendar/calendar.tsx
@@ -5,6 +5,7 @@ import props from './props';
 import { useTNodeJSX } from '../hooks/tnode';
 import TCalendarTemplate from './template';
 import { usePrefixClass } from '../hooks/useClass';
+import type { CalendarTrigger } from './type';
 
 const { prefix } = config;
 
@@ -34,20 +35,23 @@ export default defineComponent({
       context.emit('update:visible', v);
     };
 
-    const onPopupVisibleChange = (v: boolean) => {
-      if (!v) {
-        props.onClose?.('overlay');
-      } else {
-        nextTick(() => {
-          selectedValueIntoView();
-        });
-      }
+    const onPopupVisibleChange = (v: boolean, trigger: CalendarTrigger) => {
+      if (!v) props.onClose?.(trigger);
       context.emit('update:visible', v);
     };
 
     onMounted(() => {
       if (!props.usePopup) selectedValueIntoView();
     });
+
+    watch(
+      () => props.visible,
+      (val) => {
+        if (val) {
+          nextTick(() => selectedValueIntoView());
+        }
+      },
+    );
 
     watch(
       () => props.value,
