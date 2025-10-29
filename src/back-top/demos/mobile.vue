8b4d41c61e3ebc00e00dc15df0a4f20d84eedd3d
slatejack <1815846446@qq.com>
2025-10-29 18:19:20 +0800

    fix(back-top): fix back-top demo, remove not exist prop

--- src/back-top/demos/mobile.vue
+++ src/back-top/demos/mobile.vue
@@ -2,13 +2,13 @@
   <div ref="container" class="tdesign-mobile-demo">
     <h1 class="title">BackTop 回到顶部</h1>
     <p class="summary">当页面过长往下滑动是会出现返回顶部的便捷操作，帮助用户快速回到页面顶部</p>
-    <tdesign-demo-block title="组件类型" summary="圆形返回顶部">
+    <tdesign-demo-block summary="圆形返回顶部" title="组件类型">
       <BaseDemo v-model:text="text" v-model:theme="theme" :container="() => container?.parentElement" />
     </tdesign-demo-block>
-    <tdesign-demo-block title="" summary="半圆形返回顶部">
+    <tdesign-demo-block summary="半圆形返回顶部" title="">
       <HalfRoundDemo v-model:text="text" v-model:theme="theme" :container="() => container?.parentElement" />
     </tdesign-demo-block>
-    <t-back-top :theme="theme" :text="text" :on-to-top="handleToTop" />
+    <t-back-top :text="text" :theme="theme" />
   </div>
 </template>
 
