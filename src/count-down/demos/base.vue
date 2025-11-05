cf042e2651f14ab76bef462a829e758a6c84bb3b
slatejack <1815846446@qq.com>
2025-11-05 16:02:34 +0800

    fix(count-down): fix count-down demo style, alignment figma

--- src/count-down/demos/base.vue
+++ src/count-down/demos/base.vue
@@ -1,43 +1,43 @@
 <template>
   <div class="demo-count-down">
-    <text class="demo-count-down-desc"> 时分秒 </text>
+    <text class="demo-count-down-desc"> 时分秒</text>
     <div class="demo-count-down-content">
       <t-count-down :time="time" />
     </div>
   </div>
 
   <div class="demo-count-down">
-    <text class="demo-count-down-desc"> 带毫秒 </text>
+    <text class="demo-count-down-desc"> 带毫秒</text>
     <div class="demo-count-down-content">
       <t-count-down :time="time" millisecond />
     </div>
   </div>
 
   <div class="demo-count-down">
-    <text class="demo-count-down-desc"> 带方形底 </text>
+    <text class="demo-count-down-desc"> 带方形底</text>
     <div class="demo-count-down-content">
-      <t-count-down content="default" :time="time" theme="square"> </t-count-down>
+      <t-count-down :time="time" content="default" theme="square"></t-count-down>
     </div>
   </div>
 
   <div class="demo-count-down">
-    <text class="demo-count-down-desc"> 带圆形底 </text>
+    <text class="demo-count-down-desc"> 带圆形底</text>
     <div class="demo-count-down-content">
-      <t-count-down :time="time" theme="round"> </t-count-down>
+      <t-count-down :time="time" theme="round"></t-count-down>
     </div>
   </div>
 
   <div class="demo-count-down">
-    <text class="demo-count-down-desc"> 带单位 </text>
+    <text class="demo-count-down-desc"> 带单位</text>
     <div class="demo-count-down-content">
       <t-count-down :time="time" split-with-unit theme="round" />
     </div>
   </div>
 
   <div class="demo-count-down">
-    <text class="demo-count-down-desc"> 无底色带单位 </text>
+    <text class="demo-count-down-desc"> 无底色带单位</text>
     <div class="demo-count-down-content">
-      <t-count-down class="custom" :time="time" split-with-unit />
+      <t-count-down :time="time" class="custom" split-with-unit />
     </div>
   </div>
 </template>
@@ -55,6 +55,8 @@ const time = 96 * 60 * 1000;
 </script>
 
 <style lang="less">
+@import '../../_common/style/mobile/_variables';
+
 .demo-count-down-desc {
   color: var(--td-text-color-secondary, rgba(0, 0, 0, 0.6));
   font-size: 14px;
@@ -66,7 +68,7 @@ const time = 96 * 60 * 1000;
 
 // 覆盖组件内部样式
 .custom .t-count-down__item {
-  color: #e34d59;
+  color: @error-color;
   font-size: 18px;
   line-height: 24px;
   vertical-align: middle;
