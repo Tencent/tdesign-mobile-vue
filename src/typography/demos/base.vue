492c9378a3f7021abb694c1bc5feb335a0de7e9b
anlyyao <anly_yaw@163.com>
2026-04-22 21:11:32 +0800

    feat: update demo

---
+++ src/typography/demos/base.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="tdesign-mobile-typography-demo">
+    <t-typography-text> TDesign 是腾讯各业务团队在服务业务过程中沉淀的一套企业级设计体系。 </t-typography-text>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.tdesign-mobile-typography-demo {
+  display: flex;
+  padding: 0 16px;
+  background-color: var(--bg-color-demo, #fff);
+}
+</style>
