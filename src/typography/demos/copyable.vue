77619581f6370ef695f6960618773ed5d49a793e
anlyyao <anly_yaw@163.com>
2026-04-22 21:11:32 +0800

    feat: update demo

---
+++ src/typography/demos/copyable.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div class="tdesign-mobile-typography-demo">
+      <t-typography-text copyable>This is a copyable text.</t-typography-text>
+    </div>
+    <div class="tdesign-mobile-typography-demo">
+      <t-typography-text :copyable="copyableWithCustomSuffix">
+        This is a copyable text with custom icon.
+      </t-typography-text>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { SmileIcon } from 'tdesign-icons-vue-next';
+
+const copyableWithCustomSuffix = {
+  suffix: (h: typeof import('vue').h) => h(SmileIcon, { color: '#00529D' }),
+  onCopy: () => {
+    console.log('触发 copy 事件，已复制文本');
+  },
+};
+</script>
+
+<style scoped lang="less">
+.tdesign-mobile-typography-demo {
+  position: relative;
+  display: flex;
+  padding: 0 16px;
+  background-color: var(--bg-color-demo, #fff);
+
+  &:not(:last-child)::after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 16px;
+    right: 16px;
+    border-bottom: 1px solid var(--td-stroke-color, #e7e7e7);
+    transform: scaleY(0.5);
+  }
+}
+</style>
---
+++ src/typography/demos/theme.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="tdesign-mobile-typography-demo">
+    <t-typography-text theme="primary">常规</t-typography-text>
+    <t-typography-text theme="secondary">主色</t-typography-text>
+    <t-typography-text theme="success">成功</t-typography-text>
+    <t-typography-text theme="warning">警告</t-typography-text>
+    <t-typography-text theme="error">错误</t-typography-text>
+    <t-typography-text mark>标记</t-typography-text>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.tdesign-mobile-typography-demo {
+  padding: 0 16px;
+  background-color: var(--bg-color-demo, #fff);
+  display: flex;
+  gap: 24px;
+}
+</style>
