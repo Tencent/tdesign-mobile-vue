1f7daea9ca7fae77f4bbaa11ab0835437d1f6409
anlyyao <anly_yaw@163.com>
2026-09-09 21:55:11 +0800

    fix(swiper): fix navigation prop type warning when passing boolean

--- src/swiper/type.ts
+++ src/swiper/type.ts
@@ -58,8 +58,9 @@ export interface TdSwiperProps {
   loop?: boolean;
   /**
    * 导航器全部配置
+   * @default true
    */
-  navigation?: SwiperNavigation | TNode;
+  navigation?: SwiperNavigation | TNode | Boolean;
   /**
    * 【开发中】后边距，可用于露出后一项的一小部分。默认单位 `px`
    * @default 0
