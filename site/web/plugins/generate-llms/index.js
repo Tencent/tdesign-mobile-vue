0088b499c54636d7f4d20dca71ed6addc5a7c453
黎伟杰 <674416404@qq.com>
2026-09-10 11:38:50 +0800

    chore(generate-llms): 添加 siteBaseUrl 配置并更新 _common 子模块

--- site/web/plugins/generate-llms/index.js
+++ site/web/plugins/generate-llms/index.js
@@ -103,6 +103,7 @@ export default function generateLlmsPlugin() {
         parseComponentDoc,
         siteTitle: 'TDesign Mobile Vue',
         siteDescription: 'TDesign Mobile Vue 组件库的 LLM 友好文档索引。',
+        siteBaseUrl: config.base,
       });
     },
   };
