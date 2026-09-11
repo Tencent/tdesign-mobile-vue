e26e09fbcd6705f27cbc75421521ebc605b7f997
黎伟杰 <674416404@qq.com>
2026-09-11 09:38:05 +0800

    docs: 更新移动端组件库描述信息

--- site/web/plugins/generate-llms/index.js
+++ site/web/plugins/generate-llms/index.js
@@ -102,7 +102,7 @@ export default function generateLlmsPlugin() {
         platform: 'mobile',
         parseComponentDoc,
         siteTitle: 'TDesign Mobile Vue',
-        siteDescription: 'TDesign Mobile Vue 组件库的 LLM 友好文档索引。',
+        siteDescription: 'TDesign 适配移动端的组件库，适合在 Vue3.x 技术栈项目中使用。',
         siteBaseUrl: config.base,
       });
     },
