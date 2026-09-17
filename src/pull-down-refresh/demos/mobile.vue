972ffb971f988f4b619a171f8f292916e10aad3f
liweijie <674416404@qq.com>
2026-09-17 23:31:54 +0800

    chore(pull-down-refresh): 移除移动端演示中的多余示例

--- src/pull-down-refresh/demos/mobile.vue
+++ src/pull-down-refresh/demos/mobile.vue
@@ -1,15 +1,11 @@
 <template>
   <div>
     <BaseDemo />
-    <TimeoutDemo />
-    <LoadingTextsDemo />
   </div>
 </template>
 
 <script lang="ts" setup>
 import BaseDemo from './base.vue';
-import TimeoutDemo from './timeout.vue';
-import LoadingTextsDemo from './loading-texts.vue';
 </script>
 
 <style lang="less">
