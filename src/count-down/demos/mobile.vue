30e5523679624c1a12d7236bb0e7d79aa0f6472c
黎伟杰 <674416404@qq.com>
2026-09-22 15:53:17 +0800

    docs(count-down): 补充移动端基础倒计时示例标题

--- src/count-down/demos/mobile.vue
+++ src/count-down/demos/mobile.vue
@@ -2,7 +2,7 @@
   <div class="tdesign-mobile-demo">
     <h1 class="title">CountDown 倒计时</h1>
     <p class="summary">用于实时展示倒计时数值。</p>
-    <tdesign-demo-block title="01 组件类型" :padding="true">
+    <tdesign-demo-block title="01 组件类型" summary="基础倒计时" :padding="true">
       <baseDemo />
     </tdesign-demo-block>
     <tdesign-demo-block title="02 组件尺寸" summary="倒计时 large/medium/small 尺寸" :padding="true">
